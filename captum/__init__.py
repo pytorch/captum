b0054fc3905687026ad9123683e04ba4bf5960c8
Orion Reblitz-Richardson <orionr@gmail.com>
2019-08-28 16:12:09 -0700

    Switch to attr and put algorithms under _core

--- captum/__init__.py
+++ captum/__init__.py
@@ -1,30 +1,3 @@
 #!/usr/bin/env python3
 
-from ._attribution.integrated_gradients import IntegratedGradients  # noqa
-from ._attribution.deep_lift import DeepLift  # noqa
-from ._attribution.input_x_gradient import InputXGradient  # noqa
-from ._attribution.saliency import Saliency  # noqa
-from ._attribution.noise_tunnel import NoiseTunnel  # noqa
-from ._attribution.models.base import (
-    InterpretableEmbeddingBase,
-    TokenReferenceBase,
-    configure_interpretable_embedding_layer,
-)  # noqa
-
-from ._attribution.utils.gradient import compute_gradients  # noqa
-from ._attribution.utils import visualization  # noqa
-
 __version__ = "0.1.0"
-
-__all__ = [
-    "IntegratedGradients",
-    "DeepLift",
-    "InputXGradient",
-    "Saliency",
-    "NoiseTunnel",
-    "InterpretableEmbeddingBase",
-    "TokenReferenceBase",
-    "compute_gradients",
-    "visualization",
-    "configure_interpretable_embedding_layer",
-]
