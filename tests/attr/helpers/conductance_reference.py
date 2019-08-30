Fixing tests to not require gradients

tests/attr/helpers/conductance_reference.py
@@ -4,6 +4,7 @@ import torch
 from captum.attr._utils.approximation_methods import approximation_parameters
 from captum.attr._utils.attribution import LayerAttribution
 from captum.attr._utils.common import _reshape_and_sum
+from captum.attr._utils.gradient import prepare_gradient_inputs, undo_gradient_requirements
 
 
 """
@@ -137,7 +138,7 @@ class ConductanceReference(LayerAttribution):
         """
         if baselines is None:
             baselines = 0
-
+        gradient_mask = prepare_gradient_inputs((inputs,))
         # retrieve step size and scaling factor for specified approximation method
         step_sizes_func, alphas_func = approximation_parameters(method)
         step_sizes, alphas = step_sizes_func(n_steps), alphas_func(n_steps)
@@ -171,6 +172,8 @@ class ConductanceReference(LayerAttribution):
         scaled_grads = mid_layer_gradients.contiguous().view(
             n_steps, -1
         ) * torch.tensor(step_sizes).view(n_steps, 1).to(mid_layer_gradients.device)
+
+        undo_gradient_requirements((inputs,), gradient_mask)
 
         # Element-wise mutliply gradient of output with respect to hidden layer
         # and summed gradients with respect to input (chain rule) and sum across

tests/attr/test_layer_conductance.py
@@ -13,7 +13,7 @@ from .helpers.utils import assertArraysAlmostEqual
 class Test(unittest.TestCase):
     def test_simple_input_conductance(self):
         net = TestModel_MultiLayer()
-        inp = torch.tensor([[0.0, 100.0, 0.0]], requires_grad=True)
+        inp = torch.tensor([[0.0, 100.0, 0.0]])
         self._conductance_test_helper(net, net.linear0, inp, [0.0, 390.0, 0.0])
 
     def test_simple_linear_conductance(self):
@@ -25,7 +25,7 @@ class Test(unittest.TestCase):
 
     def test_simple_relu_conductance(self):
         net = TestModel_MultiLayer()
-        inp = torch.tensor([[0.0, 100.0, 0.0]], requires_grad=True)
+        inp = torch.tensor([[0.0, 100.0, 0.0]])
         self._conductance_test_helper(net, net.relu, inp, [90.0, 100.0, 100.0, 100.0])
 
     def test_simple_output_conductance(self):
@@ -40,7 +40,7 @@ class Test(unittest.TestCase):
 
     def test_matching_pool1_conductance(self):
         net = TestModel_ConvNet()
-        inp = 100 * torch.randn(1, 1, 10, 10, requires_grad=True)
+        inp = 100 * torch.randn(1, 1, 10, 10)
         self._conductance_reference_test_helper(net, net.pool1, inp)
 
     def test_matching_conv2_conductance(self):
@@ -50,7 +50,7 @@ class Test(unittest.TestCase):
 
     def test_matching_pool2_conductance(self):
         net = TestModel_ConvNet()
-        inp = 100 * torch.randn(1, 1, 10, 10, requires_grad=True)
+        inp = 100 * torch.randn(1, 1, 10, 10)
         self._conductance_reference_test_helper(net, net.pool2, inp)
 
     def test_matching_conv_multi_input_conductance(self):
@@ -60,7 +60,7 @@ class Test(unittest.TestCase):
 
     def test_matching_conv_with_baseline_conductance(self):
         net = TestModel_ConvNet()
-        inp = 100 * torch.randn(3, 1, 10, 10, requires_grad=True)
+        inp = 100 * torch.randn(3, 1, 10, 10)
         baseline = 100 * torch.randn(3, 1, 10, 10, requires_grad=True)
         self._conductance_reference_test_helper(net, net.fc1, inp, baseline)
 

tests/attr/test_layer_gradient_x_activation.py
@@ -17,7 +17,7 @@ class Test(unittest.TestCase):
 
     def test_simple_linear_gradient_activation(self):
         net = TestModel_MultiLayer()
-        inp = torch.tensor([[0.0, 100.0, 0.0]], requires_grad=True)
+        inp = torch.tensor([[0.0, 100.0, 0.0]])
         self._layer_activation_test_helper(
             net, net.linear1, inp, [90.0, 101.0, 101.0, 101.0]
         )
@@ -29,7 +29,7 @@ class Test(unittest.TestCase):
 
     def test_simple_output_gradient_activation(self):
         net = TestModel_MultiLayer()
-        inp = torch.tensor([[0.0, 100.0, 0.0]], requires_grad=True)
+        inp = torch.tensor([[0.0, 100.0, 0.0]])
         self._layer_activation_test_helper(net, net.linear2, inp, [392.0, 0.0])
 
     def _layer_activation_test_helper(

tests/attr/test_neuron_gradient.py
@@ -20,7 +20,7 @@ class Test(unittest.TestCase):
 
     def test_simple_gradient_input_linear1(self):
         net = TestModel_MultiLayer()
-        inp = torch.tensor([[0.0, 100.0, 0.0]], requires_grad=True)
+        inp = torch.tensor([[0.0, 100.0, 0.0]])
         self._gradient_input_test_assert(net, net.linear1, inp, (0,), [1.0, 1.0, 1.0])
 
     def test_simple_gradient_input_relu(self):
@@ -30,7 +30,7 @@ class Test(unittest.TestCase):
 
     def test_simple_gradient_input_relu2(self):
         net = TestModel_MultiLayer()
-        inp = torch.tensor([[0.0, 5.0, 4.0]], requires_grad=True)
+        inp = torch.tensor([[0.0, 5.0, 4.0]])
         self._gradient_input_test_assert(net, net.relu, inp, 1, [1.0, 1.0, 1.0])
 
     def test_matching_output_gradient(self):
@@ -40,7 +40,7 @@ class Test(unittest.TestCase):
 
     def test_matching_intermediate_gradient(self):
         net = TestModel_ConvNet()
-        inp = torch.randn(3, 1, 10, 10, requires_grad=True)
+        inp = torch.randn(3, 1, 10, 10)
         self._gradient_matching_test_assert(net, net.relu2, inp)
 
     def _gradient_input_test_assert(