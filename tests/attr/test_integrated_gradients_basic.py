Fixing tests to not require gradients

tests/attr/test_integrated_gradients_basic.py
@@ -65,7 +65,7 @@ class Test(unittest.TestCase):
     def _assert_multi_variable(self, type):
         model = BasicModel2()
 
-        input1 = torch.tensor([3.0], requires_grad=True)
+        input1 = torch.tensor([3.0])
         input2 = torch.tensor([1.0], requires_grad=True)
 
         baseline1 = torch.tensor([0.0])
@@ -100,7 +100,7 @@ class Test(unittest.TestCase):
         )
         self._compute_attribution_and_evaluate(
             model,
-            torch.tensor([0.0], requires_grad=True),
+            torch.tensor([0.0]),
             torch.tensor([0.0]),
             type=type,
         )

tests/attr/test_internal_influence.py
@@ -17,7 +17,7 @@ class Test(unittest.TestCase):
 
     def test_simple_linear_internal_inf(self):
         net = TestModel_MultiLayer()
-        inp = torch.tensor([[0.0, 100.0, 0.0]], requires_grad=True)
+        inp = torch.tensor([[0.0, 100.0, 0.0]])
         self._internal_influence_test_helper(
             net, net.linear1, inp, [[0.9, 1.0, 1.0, 1.0]]
         )
@@ -29,13 +29,13 @@ class Test(unittest.TestCase):
 
     def test_simple_output_internal_inf(self):
         net = TestModel_MultiLayer()
-        inp = torch.tensor([[0.0, 100.0, 0.0]], requires_grad=True)
+        inp = torch.tensor([[0.0, 100.0, 0.0]])
         self._internal_influence_test_helper(net, net.linear2, inp, [[1.0, 0.0]])
 
     def test_simple_with_baseline_internal_inf(self):
         net = TestModel_MultiLayer()
-        inp = torch.tensor([[0.0, 80.0, 0.0]], requires_grad=True)
-        base = torch.tensor([[0.0, -20.0, 0.0]], requires_grad=True)
+        inp = torch.tensor([[0.0, 80.0, 0.0]])
+        base = torch.tensor([[0.0, -20.0, 0.0]])
         self._internal_influence_test_helper(
             net, net.linear1, inp, [[0.7, 0.8, 0.8, 0.8]], base
         )

tests/attr/test_neuron_conductance.py
@@ -20,7 +20,7 @@ class Test(unittest.TestCase):
 
     def test_simple_conductance_input_linear1(self):
         net = TestModel_MultiLayer()
-        inp = torch.tensor([[0.0, 100.0, 0.0]], requires_grad=True)
+        inp = torch.tensor([[0.0, 100.0, 0.0]])
         self._conductance_input_test_assert(net, net.linear1, inp, 0, [0.0, 90.0, 0.0])
 
     def test_simple_conductance_input_relu(self):
@@ -30,7 +30,7 @@ class Test(unittest.TestCase):
 
     def test_matching_conv2_multi_input_conductance(self):
         net = TestModel_ConvNet()
-        inp = 100 * torch.randn(2, 1, 10, 10, requires_grad=True)
+        inp = 100 * torch.randn(2, 1, 10, 10)
         self._conductance_input_sum_test_assert(net, net.conv2, inp)
 
     def test_matching_relu2_multi_input_conductance(self):
@@ -41,7 +41,7 @@ class Test(unittest.TestCase):
 
     def test_matching_pool2_multi_input_conductance(self):
         net = TestModel_ConvNet()
-        inp = 100 * torch.randn(1, 1, 10, 10, requires_grad=True)
+        inp = 100 * torch.randn(1, 1, 10, 10)
         baseline = 20 * torch.randn(1, 1, 10, 10, requires_grad=True)
         self._conductance_input_sum_test_assert(net, net.pool2, inp, baseline)
 

tests/attr/test_neuron_integrated_gradients.py
@@ -13,7 +13,7 @@ from .helpers.utils import assertArraysAlmostEqual
 class Test(unittest.TestCase):
     def test_simple_ig_input_linear2(self):
         net = TestModel_MultiLayer()
-        inp = torch.tensor([[0.0, 100.0, 0.0]], requires_grad=True)
+        inp = torch.tensor([[0.0, 100.0, 0.0]])
         self._ig_input_test_assert(net, net.linear2, inp, 0, [0.0, 390.0, 0.0])
 
     def test_simple_ig_input_linear1(self):
@@ -28,7 +28,7 @@ class Test(unittest.TestCase):
 
     def test_simple_ig_input_relu2(self):
         net = TestModel_MultiLayer()
-        inp = torch.tensor([[0.0, 5.0, 4.0]], requires_grad=True)
+        inp = torch.tensor([[0.0, 5.0, 4.0]])
         self._ig_input_test_assert(net, net.relu, inp, 1, [0.0, 5.0, 4.0])
 
     def test_matching_output_gradient(self):

tests/attr/test_saliency.py
@@ -31,7 +31,7 @@ class Test(unittest.TestCase):
         self._saliency_classification_helper(reg_type="vargrad")
 
     def _saliency_base_helper(self, reg_type="vanilla"):
-        input = torch.tensor([1.0, 2.0, 3.0, 0.0, -1.0, 7.0], requires_grad=True)
+        input = torch.tensor([1.0, 2.0, 3.0, 0.0, -1.0, 7.0])
         # manually percomputed gradients
         grads = torch.tensor([-0.0, -0.0, -0.0, 1.0, 1.0, -0.0])
         model = BasicModel()