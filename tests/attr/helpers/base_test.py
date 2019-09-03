5957ec53a1b85282b000437dde173c7e1eeb2319
Vivek Miglani <vivekm@fb.com>
2019-09-03 09:25:26 -0700

    Adding comment

--- tests/attr/helpers/base_test.py
+++ tests/attr/helpers/base_test.py
@@ -3,5 +3,10 @@ import unittest
 import torch
 
 class BaseTest(unittest.TestCase):
+    """
+    This class provides a basic framework for all Captum tests by providing
+    a set up fixture, which sets a fixed random seed. Since many torch
+    initializations are random, this ensures that tests run deterministically.
+    """
     def setUp(self):
         torch.manual_seed(1234)
