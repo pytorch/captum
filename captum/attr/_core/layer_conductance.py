2aaec99e22ee3ad8caa179a4d3d952e8c072a6ee
Narine Kokhlikyan <narine.kokhlikyan@gmail.com>
2019-09-17 15:31:55 -0700

    Address comments

--- captum/attr/_core/layer_conductance.py
+++ captum/attr/_core/layer_conductance.py
@@ -62,7 +62,7 @@ class LayerConductance(LayerAttribution):
                 attributions: Total conductance with respect to each neuron in
                               output of given layer
                 delta (float): The difference between the total approximated and
-                            true integrated gradients.
+                            true conductance.
                             This is computed using the property that the total sum of
                             forward_func(inputs) - forward_func(baselines) must equal
                             the total sum of the attributions.
