0ec1cc73540ac395ef76a7aaa25d135bbe8e770a
Edward Wang <hack@fb.com>
2019-09-25 18:18:32 -0700

    make linter happy

--- captum/insights/api.py
+++ captum/insights/api.py
@@ -81,9 +81,7 @@ class AttributionVisualizer(object):
         pred_scores = []
         for i in range(len(indices)):
             score = scores[i].item()
-            pred_scores.append(
-                PredictionScore(score, self.classes[indices[i].item()])
-            )
+            pred_scores.append(PredictionScore(score, self.classes[indices[i].item()]))
         return pred_scores
 
     def _transform(
