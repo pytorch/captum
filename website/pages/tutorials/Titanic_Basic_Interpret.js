247c2da99d6e803440d1ee3b2d827e045cc8c0a7
Jessica Lin <jlin2700@gmail.com>
2019-09-25 17:00:17 -0700

    Add tutorial json files generated from build script
    Do not add these into the master branch. This is only meant for the netlify preview.

---
+++ website/pages/tutorials/Titanic_Basic_Interpret.js
@@ -0,0 +1,15 @@
+const CWD = process.cwd();
+
+const React = require('react');
+const Tutorial = require(`${CWD}/core/Tutorial.js`);
+
+class TutorialPage extends React.Component {
+  render() {
+      const {config: siteConfig} = this.props;
+      const {baseUrl} = siteConfig;
+      return <Tutorial baseUrl={baseUrl} tutorialID="Titanic_Basic_Interpret"/>;
+  }
+}
+
+module.exports = TutorialPage;
+
