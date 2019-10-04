ef9ef8df8ae4bcb3b3f58f32f604c493e8ee29de
Vivek Miglani <vivekm@fb.com>
2019-10-04 11:52:46 -0700

    Fixing interpolate method

--- captum/attr/_utils/attribution.py
+++ captum/attr/_utils/attribution.py
@@ -134,7 +134,7 @@ class LayerAttribution(InternalAttribution):
         super().__init__(forward_func, layer)
 
     def interpolate(layer_attribution, interpolate_dims):
-        return F.interpolate(layer_attribution, interpolate_dims)
+        return F.interpolate(layer_attribution, interpolate_dims, mode="bilinear")
 
 
 class NeuronAttribution(InternalAttribution):
