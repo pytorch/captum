c50ce4d0af31439a4829e9e3f5893b390f3e8772
Vivek Miglani <vivekm@fb.com>
2019-10-23 14:38:28 -0700

    Removing future

--- tests/attr/test_integrated_gradients_basic.py
+++ tests/attr/test_integrated_gradients_basic.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-from __future__ import print_function
 
 from captum.attr._core.integrated_gradients import IntegratedGradients
 from captum.attr._core.noise_tunnel import NoiseTunnel
--- tests/insights/test_contribution.py
+++ tests/insights/test_contribution.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-from __future__ import print_function
 
 import unittest
 from typing import Callable, List, Optional, Union
