0514044ba395ac8f87e1059e6b492a808d5b3c95
Vivek Miglani <vivekm@fb.com>
2019-11-08 11:32:36 -0800

    Fixing test

--- tests/attr/test_layer_activation.py
+++ tests/attr/test_layer_activation.py
@@ -3,13 +3,14 @@
 import unittest
 
 import torch
+import torch.nn as nn
 from captum.attr._core.layer_activation import LayerActivation
 
 from .helpers.basic_models import (
     BasicModel_MultiLayer,
     BasicModel_MultiLayer_MultiInput,
 )
-from .helpers.utils import assertArraysAlmostEqual, BaseTest
+from .helpers.utils import assertArraysAlmostEqual, BaseTest, assertTensorAlmostEqual
 
 
 class Test(BaseTest):
@@ -28,9 +29,7 @@ class Test(BaseTest):
     def test_simple_relu_activation_input_inplace(self):
         net = BasicModel_MultiLayer(inplace=True)
         inp = torch.tensor([[2.0, -5.0, 4.0]])
-        self._layer_activation_test_assert(
-            net, net.relu, inp, [-9.0, 2.0, 2.0, 2.0], attribute_to_layer_input=True
-        )
+        self._layer_activation_test_assert(net, net.relu, inp, [-9.0, 2.0, 2.0, 2.0], attribute_to_layer_input=True)
 
     def test_simple_linear_activation_inplace(self):
         net = BasicModel_MultiLayer(inplace=True)
@@ -65,6 +64,14 @@ class Test(BaseTest):
             net, net.model.relu, (inp1, inp2), [90.0, 101.0, 101.0, 101.0], (inp3, 5)
         )
 
+    def test_sequential_in_place(self):
+        model = nn.Sequential(nn.Conv2d(3, 4, 3),
+                              nn.ReLU(inplace=True))
+        layer_act = LayerActivation(model, model[0])
+        input = torch.randn(1, 3, 5, 5)
+        assertTensorAlmostEqual(self, layer_act.attribute(input), model[0](input))
+
+
     def _layer_activation_test_assert(
         self,
         model,
@@ -76,9 +83,7 @@ class Test(BaseTest):
     ):
         layer_act = LayerActivation(model, target_layer)
         attributions = layer_act.attribute(
-            test_input,
-            additional_forward_args=additional_input,
-            attribute_to_layer_input=attribute_to_layer_input,
+            test_input, additional_forward_args=additional_input, attribute_to_layer_input=attribute_to_layer_input
         )
         assertArraysAlmostEqual(
             attributions.squeeze(0).tolist(), expected_activation, delta=0.01
