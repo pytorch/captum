b9010dc9144b8b003f1454d455c876e2f7e5b2d7
Miguel Martin <miguelmartin@fb.com>
2019-11-22 14:01:23 -0800

    fix bug in aggr

--- tests/attr/test_aggregator.py
+++ tests/attr/test_aggregator.py
@@ -7,20 +7,27 @@ from .helpers.utils import BaseTest, assertTensorAlmostEqual
 
 class Test(BaseTest):
     def test_single_input(self):
+        size = (2, 3)
         aggr = common_aggr()
         for _ in range(10):
-            attrs = torch.randn(10, 5, 5)
+            attrs = torch.randn(size)
             aggr.update(attrs)
 
         summ = aggr.summary
         self.assertIsNotNone(summ)
         self.assertTrue(isinstance(summ, dict))
 
+        for k in summ:
+            self.assertTrue(summ[k].size() == size)
+
     def test_multi_input(self):
+        size1 = (10, 5, 5)
+        size2 = (3, 5)
+
         aggr = common_aggr()
         for _ in range(10):
-            a1 = torch.randn(10, 5, 5)
-            a2 = torch.randn(10, 5, 5)
+            a1 = torch.randn(size1)
+            a2 = torch.randn(size2)
             aggr.update((a1, a2))
 
         summ = aggr.summary
@@ -28,3 +35,7 @@ class Test(BaseTest):
         self.assertTrue(len(summ) == 2)
         self.assertTrue(isinstance(summ[0], dict))
         self.assertTrue(isinstance(summ[1], dict))
+
+        for k in summ[0]:
+            self.assertTrue(summ[0][k].size() == size1)
+            self.assertTrue(summ[1][k].size() == size2)
