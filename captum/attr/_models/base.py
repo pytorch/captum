6bef9bf6222d5009741ed9e9600342ec7a103045
Narine Kokhlikyan <narine@fb.com>
2019-12-20 13:50:11 -0800

    Address comments

--- captum/attr/_models/base.py
+++ captum/attr/_models/base.py
@@ -42,13 +42,19 @@ class InterpretableEmbeddingBase(Module):
                     execute the forward pass using interpretable embedding layer.
                     Note that if inputs are specified, it is assumed that the first
                     argument is the embedding tensor generated using the
-                    `self.embedding` layer using all imput arguments provided in
+                    `self.embedding` layer using all input arguments provided in
                     `inputs` and `kwargs`.
             **kwargs (Any, optional): Similar to `inputs` we want to make sure
                     that our forward pass supports arbitrary number and type of
                     key-value arguments. If `inputs` is not provided, `kwargs` must
-                    be provded and the first argument corresponds to the embedding
-                    tensor generated using the `self.embedding`.
+                    be provided and the first argument corresponds to the embedding
+                    tensor generated using the `self.embedding`. Note that we make
+                    here an assumption here that `kwargs` is an ordered dict which
+                    is new in python 3.6 and is not guaranteed that it will
+                    consistently remain that way in the newer versions. In case
+                    current implementation doesn't work for special use cases,
+                    it is encouraged to override `InterpretableEmbeddingBase` and
+                    address those specifics in descendant classes.
 
          Returns:
 
