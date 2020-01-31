599d92b97ba72c6475c10ce64de0e924677c960b
Lucia Li <luciali@fb.com>
2020-01-31 14:57:09 -0800

    add type hint to input_x_gradient

--- captum/attr/_core/input_x_gradient.py
+++ captum/attr/_core/input_x_gradient.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python3
+from typing import Callable, List, Optional, Tuple, Union, Any
+from torch import Tensor
+
 from .._utils.common import _format_input, _format_attributions
 from .._utils.attribution import GradientAttribution
 from .._utils.gradient import apply_gradient_requirements, undo_gradient_requirements
 
 
 class InputXGradient(GradientAttribution):
-    def __init__(self, forward_func):
+    def __init__(self, forward_func: Callable) -> None:
         r"""
         Args:
 
@@ -14,7 +17,14 @@ class InputXGradient(GradientAttribution):
         """
         GradientAttribution.__init__(self, forward_func)
 
-    def attribute(self, inputs, target=None, additional_forward_args=None):
+    def attribute(
+        self,
+        inputs: Union[Tensor, Tuple[Tensor, ...]],
+        target: Optional[
+            Union[int, Tuple[int, ...], Tensor, List[Tuple[int, ...]]]
+        ] = None,
+        additional_forward_args: Any = None,
+    ) -> Union[Tensor, Tuple[Tensor, ...]]:
         r""""
         A baseline approach for computing the attribution. It multiplies input with
         the gradient with respect to input.
--- tests/attr/test_input_x_gradient.py
+++ tests/attr/test_input_x_gradient.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
-
+from typing import Tuple, Union, Any
 import torch
+from torch import Tensor
+from torch.nn import Module
 
 from captum.attr._core.input_x_gradient import InputXGradient
 from captum.attr._core.noise_tunnel import NoiseTunnel
@@ -11,45 +13,45 @@ from .test_saliency import _get_basic_config, _get_multiargs_basic_config
 
 
 class Test(BaseTest):
-    def test_input_x_gradient_test_basic_vanilla(self):
+    def test_input_x_gradient_test_basic_vanilla(self) -> None:
         self._input_x_gradient_base_assert(*_get_basic_config())
 
-    def test_input_x_gradient_test_basic_smoothgrad(self):
+    def test_input_x_gradient_test_basic_smoothgrad(self) -> None:
         self._input_x_gradient_base_assert(*_get_basic_config(), nt_type="smoothgrad")
 
-    def test_input_x_gradient_test_basic_vargrad(self):
+    def test_input_x_gradient_test_basic_vargrad(self) -> None:
         self._input_x_gradient_base_assert(*_get_basic_config(), nt_type="vargrad")
 
-    def test_saliency_test_basic_multi_variable_vanilla(self):
+    def test_saliency_test_basic_multi_variable_vanilla(self) -> None:
         self._input_x_gradient_base_assert(*_get_multiargs_basic_config())
 
-    def test_saliency_test_basic_multi_variable_smoothgrad(self):
+    def test_saliency_test_basic_multi_variable_smoothgrad(self) -> None:
         self._input_x_gradient_base_assert(
             *_get_multiargs_basic_config(), nt_type="smoothgrad"
         )
 
-    def test_saliency_test_basic_multi_vargrad(self):
+    def test_saliency_test_basic_multi_vargrad(self) -> None:
         self._input_x_gradient_base_assert(
             *_get_multiargs_basic_config(), nt_type="vargrad"
         )
 
-    def test_input_x_gradient_classification_vanilla(self):
+    def test_input_x_gradient_classification_vanilla(self) -> None:
         self._input_x_gradient_classification_assert()
 
-    def test_input_x_gradient_classification_smoothgrad(self):
+    def test_input_x_gradient_classification_smoothgrad(self) -> None:
         self._input_x_gradient_classification_assert(nt_type="smoothgrad")
 
-    def test_input_x_gradient_classification_vargrad(self):
+    def test_input_x_gradient_classification_vargrad(self) -> None:
         self._input_x_gradient_classification_assert(nt_type="vargrad")
 
     def _input_x_gradient_base_assert(
         self,
-        model,
-        inputs,
-        expected_grads,
-        additional_forward_args=None,
-        nt_type="vanilla",
-    ):
+        model: Module,
+        inputs: Union[Tensor, Tuple[Tensor, ...]],
+        expected_grads: Tensor,
+        additional_forward_args: Any = None,
+        nt_type: str = "vanilla",
+    ) -> None:
         input_x_grad = InputXGradient(model)
         if nt_type == "vanilla":
             attributions = input_x_grad.attribute(
@@ -81,9 +83,12 @@ class Test(BaseTest):
                     (expected_grads * inputs).reshape(-1),
                     delta=0.5,
                 )
-            self.assertEqual(inputs.shape, attributions.shape)
+            if isinstance(inputs, Tensor):
+                self.assertEqual(inputs.shape, attributions.shape)
 
-    def _input_x_gradient_classification_assert(self, nt_type="vanilla"):
+    def _input_x_gradient_classification_assert(
+        self, nt_type: str = "vanilla",
+    ) -> None:
         num_in = 5
         input = torch.tensor([[0.0, 1.0, 2.0, 3.0, 4.0]], requires_grad=True)
         target = torch.tensor(5)
@@ -96,14 +101,16 @@ class Test(BaseTest):
             output = model(input)[:, target]
             output.backward()
             expercted = input.grad * input
-            self.assertEqual(
-                expercted.detach().numpy().tolist(),
-                attributions.detach().numpy().tolist(),
-            )
+            if isinstance(attributions, Tensor):
+                self.assertEqual(
+                    expercted.detach().numpy().tolist(),
+                    attributions.detach().numpy().tolist(),
+                )
         else:
             nt = NoiseTunnel(input_x_grad)
             attributions = nt.attribute(
                 input, nt_type=nt_type, n_samples=10, stdevs=1.0, target=target
             )
 
-        self.assertAlmostEqual(attributions.shape, input.shape)
+        if isinstance(attributions, Tensor):
+            assertArraysAlmostEqual(attributions.shape, input.shape)
