e2e67bbcc35ec4f7f3c18050d3f011cda6bc2e7d
Evgeny Taranyuk <evgenytaranyuk@fb.com>
2020-02-12 15:10:45 -0500

    Adding Type Hints to Captum: neuron_conductance.py

--- tests/attr/neuron/test_neuron_conductance.py
+++ tests/attr/neuron/test_neuron_conductance.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 import unittest
-from typing import List, Optional, Tuple, Union, Any
+from typing import List, Tuple, Union, Any
 import torch
 from torch.nn import Module
 from captum.attr._core.layer.layer_conductance import LayerConductance
@@ -144,7 +144,7 @@ class Test(BaseTest):
         model: Module,
         target_layer: Module,
         test_input: TensorOrTupleOfTensors,
-        test_baseline: Optional[Any] = None,
+        test_baseline: Any = None,
     ):
         layer_cond = LayerConductance(model, target_layer)
         attributions = layer_cond.attribute(
