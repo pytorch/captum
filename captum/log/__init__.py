69fe9c5f97d7562765fde9fb0dc7e49ad90a71e9
Miguel Martin <miguelmartin@fb.com>
2020-04-28 19:12:38 -0700

    Internal Captum Metric Logging API (#344)
    Summary:
    Pull Request resolved: https://github.com/pytorch/captum/pull/344
    
    This adds a basic logging API.
    
    Reviewed By: edward-io
    
    Differential Revision: D20485560
    
    fbshipit-source-id: d6df25256c96ea0a6f48cf1ef452e21a82fbdf5e

---
+++ captum/log/__init__.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+try:
+    from captum.log.fb.internal_log import TimedLog, log, log_usage, set_environment
+
+    __all__ = ["log", "log_usage", "TimedLog", "set_environment"]
+
+except ImportError:
+    from functools import wraps
+
+    def log(*args, **kwargs):
+        pass
+
+    # bug with mypy: https://github.com/python/mypy/issues/1153
+    class TimedLog:  # type: ignore
+        def __init__(self, *args, **kwargs):
+            pass
+
+        def __enter__(self):
+            return self
+
+        def __exit__(self, exception_type, exception_value, traceback):
+            return exception_value is not None
+
+    def log_usage(*log_args, **log_kwargs):
+        def _log_usage(func):
+            @wraps(func)
+            def wrapper(*args, **kwargs):
+                return func(*args, **kwargs)
+
+            return wrapper
+
+        return _log_usage
+
+    def set_environment(env):
+        pass
