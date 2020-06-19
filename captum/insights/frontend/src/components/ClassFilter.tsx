18d4d65c3d7488bc743e4c7ba620b955c4eb53c1
Shubham Muttepawar <smuttepawar@fb.com>
2020-06-19 17:20:30 -0400

    Adding TS changes to ClassFilter

--- captum/insights/frontend/src/components/ClassFilter.tsx
+++ captum/insights/frontend/src/components/ClassFilter.tsx
@@ -1,14 +1,35 @@
 import React from "react";
 import ReactTags from "react-tag-autocomplete";
+import { TagClass } from "../models/filter";
 
-function ClassFilter(props) {
+interface ClassFilterProps {
+  suggestedClasses: TagClass[];
+  classes: TagClass[];
+  handleClassDelete: (classId: number) => void;
+  handleClassAdd: (newClass: TagClass) => void;
+}
+
+function ClassFilter(props: ClassFilterProps) {
+
+  const handleAddition = (newTag: { id: number | string, name: string }) => {
+    /**
+     * Need this type check as we expect tagId to be number while the `react-tag-autocomplete` has 
+     * id as number | string. 
+     */
+    if(typeof newTag.id === 'string') {
+      throw Error("Invalid tag id received from ReactTags");
+    } else {
+      props.handleClassAdd({id: newTag.id, name: newTag.name}); 
+    }
+  }
+
   return (
     <ReactTags
       tags={props.classes}
       autofocus={false}
       suggestions={props.suggestedClasses}
       handleDelete={props.handleClassDelete}
-      handleAddition={props.handleClassAdd}
+      handleAddition={handleAddition}
       minQueryLength={0}
       placeholder="add new class..."
     />
