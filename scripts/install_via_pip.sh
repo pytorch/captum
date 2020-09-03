c6f2e30df7e67c81b66d79b9c625cc90915a3e5a
Vivek Miglani <vivekm@fb.com>
2020-09-03 10:29:52 -0700

    Fix CI Failure (#462)
    Summary:
    CircleCI pip install tests are currently failing with this error:
    ```
    0% [Working]Err:1 http://deb.debian.org/debian stretch/main amd64 apt-transport-https amd64 1.4.9
      404  Not Found
    ```
    This requires running `sudo apt-get update `before installing `apt-transport-https`
    
    Pull Request resolved: https://github.com/pytorch/captum/pull/462
    
    Reviewed By: bilalsal
    
    Differential Revision: D23502430
    
    Pulled By: vivekmig
    
    fbshipit-source-id: 3fb06f0ae39517733740dcf114f58d70ee4097ba

--- scripts/install_via_pip.sh
+++ scripts/install_via_pip.sh
@@ -22,6 +22,7 @@ while getopts 'ndfv:' flag; do
 # but users should hopefully be using conda installs.
 
 # install nodejs and yarn for insights build
+sudo apt-get update
 sudo apt install apt-transport-https ca-certificates
 curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
