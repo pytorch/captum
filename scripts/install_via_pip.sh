7be15016874c239663df153066c53ab4501f339f
Vivek Miglani <vivekm@fb.com>
2020-09-03 07:09:02 -0700

    Test fix

--- scripts/install_via_pip.sh
+++ scripts/install_via_pip.sh
@@ -22,11 +22,11 @@ while getopts 'ndfv:' flag; do
 # but users should hopefully be using conda installs.
 
 # install nodejs and yarn for insights build
+sudo apt update
 sudo apt install apt-transport-https ca-certificates
 curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
-sudo apt update
 sudo apt install nodejs
 sudo apt install yarn
 
