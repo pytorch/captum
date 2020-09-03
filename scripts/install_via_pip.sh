4ce3fac3628dd1d759866d651a74f20f3ae4f415
Vivek Miglani <vivekm@fb.com>
2020-09-03 08:21:50 -0700

    Update

--- scripts/install_via_pip.sh
+++ scripts/install_via_pip.sh
@@ -27,6 +27,7 @@ sudo apt install apt-transport-https ca-certificates
 curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+sudo apt remove cmdtest
 sudo apt install nodejs
 sudo apt install yarn
 
