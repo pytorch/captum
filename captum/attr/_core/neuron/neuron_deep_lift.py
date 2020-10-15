7fd9172f3af5e2e92c62e4921b19b6765ca2cbc7
Vivek Miglani <vivekm@fb.com>
2020-10-15 14:41:15 -0700

    Documentation

--- captum/attr/_core/neuron/neuron_deep_lift.py
+++ captum/attr/_core/neuron/neuron_deep_lift.py
@@ -99,9 +99,9 @@ class NeuronDeepLift(NeuronAttribution, GradientAttribution):
                         corresponds to the number of examples (aka batch size),
                         and if multiple input tensors are provided, the examples
                         must be aligned appropriately.
-            neuron_selector (int or tuple): Index of neuron or neurons in output
-                        of given layer for which attribution is desired. Length
-                        of this tuple must be one less than the number of
+            neuron_selector (int or tuple or callable): Index of neuron or neurons
+                        in output of given layer for which attribution is desired.
+                        Length of this tuple must be one less than the number of
                         dimensions in the output of the given layer (since
                         dimension 0 corresponds to number of examples).
                         The elements of the tuple can be either integers or
@@ -109,6 +109,15 @@ class NeuronDeepLift(NeuronAttribution, GradientAttribution):
                         range of neurons rather individual ones).
                         An integer may be provided instead of a tuple of
                         length 1.
+                        Alternatively, a callable can be provided, which should
+                        take the target layer as input (single tensor or tuple
+                        if multiple tensors are in layer) and return a neuron or
+                        aggregate of the layer's neurons for attribution.
+                        For example, this function could return the
+                        sum of the neurons in the layer or sum of neurons with
+                        activations in a particular range. It is expected that
+                        this function returns either a tensor with one element
+                        or a scalar per example.
                         If any of the tuple elements is a slice object, the
                         indexed output tensor is used for attribution. Note
                         that specifying a slice of a tesnor would amount to
