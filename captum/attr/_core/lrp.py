Fix delta calculation for attribution tuple

captum/attr/_core/lrp.py
@@ -132,7 +132,7 @@ class LRP(GradientAttribution):
             - **delta** (*tensor*, returned if return_convergence_delta=True):
                         Delta is calculated per example, meaning that the number of
                         elements in returned delta tensor is equal to the number of
-                        of examples in input.
+                        of examples in the inputs.
         Examples::
 
                 >>> # ImageClassifier takes a single input tensor of images Nx3x32x32,
@@ -216,20 +216,17 @@ class LRP(GradientAttribution):
             - **delta** Difference of relevance in output layer and input layer.
         """
 
-        def _single_attribution_delta(attributions, output):
+        def _attribution_delta(attributions, output):
             remaining_dims = tuple(range(1, len(attributions.shape)))
             sum_attributions = torch.sum(attributions, dim=remaining_dims)
             delta = output - sum_attributions
             return delta
 
         if isinstance(attributions, tuple):
-            delta = [
-                _single_attribution_delta(attribution, output)
-                for attribution in attributions
-            ]
-            delta = tuple(delta)
+            stacked_attributions = torch.stack(attributions, dim=1)
+            delta = _attribution_delta(stacked_attributions, output)
         else:
-            delta = _single_attribution_delta(attributions, output)
+            delta = _attribution_delta(attributions, output)
         return delta
 
     def _get_layers(self, model):

tests/attr/layer/test_layer_lrp.py
@@ -69,6 +69,9 @@ class Test(BaseTest):
             inputs, classIndex.item(), return_convergence_delta=True
         )
         self.assertEqual(relevance[0].shape, torch.Size([2, 2, 2]))
+        assertTensorAlmostEqual(
+            self, delta, torch.Tensor([-21, 75])
+        )  # Due to bias in conv1 layer
 
     def test_lrp_simple_attributions(self):
         model, inputs = _get_simple_model(inplace=False)
@@ -76,10 +79,13 @@ class Test(BaseTest):
         model.linear.rule = EpsilonRule()
         model.linear2.rule = EpsilonRule()
         lrp_upper = LayerLRP(model, model.linear2)
-        relevance_upper = lrp_upper.attribute(inputs, attribute_to_layer_input=True)
+        relevance_upper, delta = lrp_upper.attribute(
+            inputs, attribute_to_layer_input=True, return_convergence_delta=True
+        )
         lrp_lower = LayerLRP(model, model.linear)
         relevance_lower = lrp_lower.attribute(inputs)
         assertTensorAlmostEqual(self, relevance_lower[0], relevance_upper[0])
+        self.assertEqual(delta.item(), 0)
 
     def test_lrp_simple_repeat_attributions(self):
         model, inputs = _get_simple_model()
@@ -135,9 +141,7 @@ class Test(BaseTest):
         model.linear2.rule = GammaRule()
         lrp = LayerLRP(model, model.linear)
         relevance = lrp.attribute(inputs)
-        assertTensorAlmostEqual(
-            self, relevance[0], torch.tensor([24, 36, 36])
-        )
+        assertTensorAlmostEqual(self, relevance[0], torch.tensor([24, 36, 36]))
 
     def test_lrp_simple_attributions_AlphaBeta(self):
         model, inputs = _get_simple_model()
@@ -165,7 +169,7 @@ class Test(BaseTest):
         model.linear.rule = EpsilonRule()
         model.linear2.rule = EpsilonRule()
         lrp = LayerLRP(model, None)
-        inputs = torch.stack((inputs, 2 * inputs))
+        inputs = torch.cat((inputs, 2 * inputs))
         relevance, delta = lrp.attribute(
             inputs, attribute_to_layer_input=True, return_convergence_delta=True
         )