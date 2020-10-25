446ccf53cd1841bf2ee0d156222ff37775efc927
ProGamerGov <ProGamerGov@users.noreply.github.com>
2020-10-25 14:12:58 -0600

    isort fix

--- captum/optim/__init__.py
+++ captum/optim/__init__.py
@@ -1,8 +1,8 @@
 """optim submodule."""
 
 from captum.optim._core import objectives  # noqa: F401
+from captum.optim._core.objectives import InputOptimization  # noqa: F401
 from captum.optim._param.image import images  # noqa: F401
 from captum.optim._param.image import transform  # noqa: F401
-from captum.optim._utils import typing  # noqa: F401
-from captum.optim._core.objectives import InputOptimization  # noqa: F401
 from captum.optim._param.image.images import ImageTensor  # noqa: F401
+from captum.optim._utils import typing  # noqa: F401
