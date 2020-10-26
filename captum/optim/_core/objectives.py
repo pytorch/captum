09df8aacf1b5ad32d1db9dd0da15c991986af710
ProGamerGov <ProGamerGov@users.noreply.github.com>
2020-10-26 09:53:52 -0600

    Fix lint error

--- captum/optim/_core/objectives.py
+++ captum/optim/_core/objectives.py
@@ -206,9 +206,11 @@ def deepdream(target: nn.Module) -> LossFunction:
     Maximize 'interestingness' at some layer.
     See Mordvintsev et al., 2015.
     """
+
     def inner(targets_to_values: ModuleOutputMapping):
         activations = targets_to_values[target]
-        return activations**2
+        return activations ** 2
+
     return inner
 
 
