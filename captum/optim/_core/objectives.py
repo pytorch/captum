184963945dafc75143b4e9659c90aa8a6abe6934
ProGamerGov <ProGamerGov@users.noreply.github.com>
2020-11-07 12:19:10 -0700

    Improve DeepDream description

--- captum/optim/_core/objectives.py
+++ captum/optim/_core/objectives.py
@@ -207,7 +207,7 @@ def neuron_activation(
 
 def deepdream(target: nn.Module) -> LossFunction:
     """
-    Maximize activations at the target layer.
+    Maximize 'interestingness' at the target layer.
     Mordvintsev et al., 2015.
     """
 
