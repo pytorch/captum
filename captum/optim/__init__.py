7c28ba1d9b92399a0066db64fb2da1800a519bab
ProGamerGov <ProGamerGov@users.noreply.github.com>
2020-11-13 15:11:39 -0700

    Oops

--- captum/optim/__init__.py
+++ captum/optim/__init__.py
@@ -3,7 +3,6 @@
 from captum.optim._core import loss  # noqa: F401
 from captum.optim._core import objectives  # noqa: F401
 from captum.optim._core.objectives import InputOptimization  # noqa: F401
-from captum.optim._core.loss import InputOptimization
 from captum.optim._param.image import images  # noqa: F401
 from captum.optim._param.image import transform  # noqa: F401
 from captum.optim._param.image.images import ImageTensor  # noqa: F401
