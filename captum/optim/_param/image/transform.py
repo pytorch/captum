c99bdd6b45c91004469fa111636ae31723c4dee4
ProGamerGov <ProGamerGov@users.noreply.github.com>
2020-11-15 10:04:16 -0700

    Add init torch.inverse() func

--- captum/optim/_param/image/transform.py
+++ captum/optim/_param/image/transform.py
@@ -74,6 +74,12 @@ class ToRGB(nn.Module):
         else:
             raise ValueError("transform_name has to be either 'klt' or 'i1i2i3'")
 
+    def decorrelate_init(self, x):
+        h, w = x.size("H"), x.size("W")
+        flat = x.flatten(("H", "W"), "spatials")
+        correct = torch.inverse(self.transform) @ flat
+        return correct.unflatten("spatials", (("H", h), ("W", w))).refine_names("C", ...)
+
     def forward(self, x, inverse=False):
         assert x.dim() == 3
 
