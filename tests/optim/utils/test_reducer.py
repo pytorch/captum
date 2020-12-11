b46e1edf3d3ae86c65daa213d1e16a602c48a40c
ProGamerGov <ProGamerGov@users.noreply.github.com>
2020-12-11 14:40:54 -0700

    Add files via upload

---
+++ tests/optim/utils/test_reducer.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+import unittest
+
+import torch
+
+from captum.optim._utils.reducer import ChannelReducer
+from tests.helpers.basic import BaseTest
+
+
+class TestChannelReducer(BaseTest):
+    def test_channelreducer(self) -> None:
+        test_input = torch.randn(1, 32, 224, 224).abs()
+        c_reducer = ChannelReducer(n_components=3, max_iter=100)
+        test_output = c_reducer.fit_transform(test_input)
+        self.assertEquals(test_output.size(1), 3)
+
+
+if __name__ == "__main__":
+    unittest.main()
