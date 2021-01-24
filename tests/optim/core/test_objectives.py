05b68de48508848deb3e216652b5b91cb0026ed6
Lim Swee Kiat <limsweekiat@gmail.com>
2021-01-24 23:28:01 +0800

    Fix test_progress

--- tests/optim/core/test_objectives.py
+++ tests/optim/core/test_objectives.py
@@ -22,7 +22,7 @@ class TestInputOptimization(BaseTest):
         model = SimpleModel()
         loss_fn = opt.loss.ChannelActivation(model.layer, 0)
         obj = opt.InputOptimization(model, loss_function=loss_fn)
-        history = obj.optimize(opt.objectives.n_steps(5))
+        history = obj.optimize(opt.objectives.n_steps(5, show_progress=False))
         history = np.mean(np.mean(history, axis=2), axis=2)
         for i, j in zip(history[:-1], history[1:]):
             assert i < j
