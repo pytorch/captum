c280fd06959dc6faf526ece1701176a49e79fa34
Lim Swee Kiat <limsweekiat@gmail.com>
2021-01-24 18:32:32 +0800

    Fix InputOptimization test

--- tests/optim/core/test_loss.py
+++ tests/optim/core/test_loss.py
@@ -1,16 +1,14 @@
 #!/usr/bin/env python3
 import unittest
-from typing import cast
 from contextlib import suppress
 
 import numpy as np
 import torch
 import torch.nn as nn
 
-from captum.optim._core.output_hook import AbortForwardException, ModuleOutputsHook
 import captum.optim._core.loss as opt_loss
+from captum.optim._core.output_hook import AbortForwardException, ModuleOutputsHook
 from tests.helpers.basic import BaseTest, assertArraysAlmostEqual
-
 
 CHANNEL_ACTIVATION_0_LOSS = -0.48031163215637207
 CHANNEL_ACTIVATION_1_LOSS = -0.2667173147201538
@@ -20,6 +18,7 @@ class SimpleModel(nn.Module):
     def __init__(self):
         super(SimpleModel, self).__init__()
         self.layer = nn.Conv2d(1, 2, 1)
+
     def forward(self, x):
         return self.layer(x)
 
@@ -40,37 +39,26 @@ class TestChannelActivation(BaseTest):
     def test_channel_activation_0(self) -> None:
         model = SimpleModel()
         loss = opt_loss.ChannelActivation(model.layer, 0)
-        self.assertAlmostEqual(
-            get_loss_value(model, loss),
-            CHANNEL_ACTIVATION_0_LOSS
-        )
+        self.assertAlmostEqual(get_loss_value(model, loss), CHANNEL_ACTIVATION_0_LOSS)
+
     def test_channel_activation_1(self) -> None:
         model = SimpleModel()
         loss = opt_loss.ChannelActivation(model.layer, 1)
-        self.assertAlmostEqual(
-            get_loss_value(model, loss),
-            CHANNEL_ACTIVATION_1_LOSS
-        )
+        self.assertAlmostEqual(get_loss_value(model, loss), CHANNEL_ACTIVATION_1_LOSS)
 
 
 class TestNeuronActivation(BaseTest):
     def test_neuron_activation_0(self) -> None:
         model = SimpleModel()
         loss = opt_loss.NeuronActivation(model.layer, 0)
-        self.assertAlmostEqual(
-            get_loss_value(model, loss),
-            CHANNEL_ACTIVATION_0_LOSS
-        )
+        self.assertAlmostEqual(get_loss_value(model, loss), CHANNEL_ACTIVATION_0_LOSS)
 
 
 class TestTotalVariation(BaseTest):
     def test_total_variation(self) -> None:
         model = SimpleModel()
         loss = opt_loss.TotalVariation(model.layer)
-        self.assertAlmostEqual(
-            get_loss_value(model, loss),
-            0.
-        )
+        self.assertAlmostEqual(get_loss_value(model, loss), 0.0)
 
 
 class TestL1(BaseTest):
@@ -79,8 +67,7 @@ class TestL1(BaseTest):
         loss = -opt_loss.L1(model.layer)
         self.assertAlmostEqual(
             get_loss_value(model, loss),
-            CHANNEL_ACTIVATION_0_LOSS + \
-            CHANNEL_ACTIVATION_1_LOSS
+            CHANNEL_ACTIVATION_0_LOSS + CHANNEL_ACTIVATION_1_LOSS,
         )
 
 
@@ -90,9 +77,8 @@ class TestL2(BaseTest):
         loss = opt_loss.L2(model.layer)
         self.assertAlmostEqual(
             get_loss_value(model, loss),
-            (CHANNEL_ACTIVATION_0_LOSS ** 2 + \
-            CHANNEL_ACTIVATION_1_LOSS ** 2) ** 0.5,
-            places=5
+            (CHANNEL_ACTIVATION_0_LOSS ** 2 + CHANNEL_ACTIVATION_1_LOSS ** 2) ** 0.5,
+            places=5,
         )
 
 
@@ -106,11 +92,8 @@ class TestActivationInterpolation(BaseTest):
             channel_index2=1,
         )
         self.assertAlmostEqual(
-            get_loss_value(model, loss,
-                input_shape=[2, 1, 1, 1]
-            ),
-            CHANNEL_ACTIVATION_0_LOSS + \
-            CHANNEL_ACTIVATION_1_LOSS
+            get_loss_value(model, loss, input_shape=[2, 1, 1, 1]),
+            CHANNEL_ACTIVATION_0_LOSS + CHANNEL_ACTIVATION_1_LOSS,
         )
 
 
@@ -119,75 +102,46 @@ class TestAlignment(BaseTest):
         model = SimpleModel()
         loss = opt_loss.Alignment(model.layer)
         self.assertAlmostEqual(
-            get_loss_value(model, loss,
-                input_shape=[2, 1, 1, 1]
-            ),
-            0.
+            get_loss_value(model, loss, input_shape=[2, 1, 1, 1]), 0.0
         )
 
 
 class TestNeuronDirection(BaseTest):
     def test_neuron_direction(self) -> None:
         model = SimpleModel()
-        loss = opt_loss.NeuronDirection(
-            model.layer,
-            vec=torch.ones(1, 1, 1, 1)
-        )
+        loss = opt_loss.NeuronDirection(model.layer, vec=torch.ones(1, 1, 1, 1))
         a = 1
-        b = [CHANNEL_ACTIVATION_0_LOSS,
-            CHANNEL_ACTIVATION_1_LOSS]
-        cos_sim = np.inner(a, b) / \
-            (np.linalg.norm(a) * np.linalg.norm(b))
+        b = [CHANNEL_ACTIVATION_0_LOSS, CHANNEL_ACTIVATION_1_LOSS]
+        cos_sim = np.inner(a, b) / (np.linalg.norm(a) * np.linalg.norm(b))
         cos_sim = np.sum(cos_sim)
-        self.assertAlmostEqual(
-            get_loss_value(model, loss),
-            cos_sim,
-            places=6
-        )
+        self.assertAlmostEqual(get_loss_value(model, loss), cos_sim, places=6)
 
 
 class TestTensorDirection(BaseTest):
     def test_tensor_direction(self) -> None:
         model = SimpleModel()
-        loss = opt_loss.TensorDirection(
-            model.layer,
-            vec=torch.ones(1, 1, 1, 1)
-        )
+        loss = opt_loss.TensorDirection(model.layer, vec=torch.ones(1, 1, 1, 1))
         a = 1
-        b = [CHANNEL_ACTIVATION_0_LOSS,
-            CHANNEL_ACTIVATION_1_LOSS]
-        cos_sim = np.inner(a, b) / \
-            (np.linalg.norm(a) * np.linalg.norm(b))
+        b = [CHANNEL_ACTIVATION_0_LOSS, CHANNEL_ACTIVATION_1_LOSS]
+        cos_sim = np.inner(a, b) / (np.linalg.norm(a) * np.linalg.norm(b))
         cos_sim = np.sum(cos_sim)
-        self.assertAlmostEqual(
-            get_loss_value(model, loss),
-            cos_sim,
-            places=6
-        )
+        self.assertAlmostEqual(get_loss_value(model, loss), cos_sim, places=6)
 
 
 class TestActivationWeights(BaseTest):
     def test_activation_weights_0(self) -> None:
         model = SimpleModel()
-        loss = opt_loss.ActivationWeights(
-            model.layer,
-            weights=torch.zeros(1)
-        )
-        assertArraysAlmostEqual(
-            get_loss_value(model, loss),
-            np.zeros((1, 2, 1))
-        )
+        loss = opt_loss.ActivationWeights(model.layer, weights=torch.zeros(1))
+        assertArraysAlmostEqual(get_loss_value(model, loss), np.zeros((1, 2, 1)))
+
     def test_activation_weights_1(self) -> None:
         model = SimpleModel()
         loss = opt_loss.ActivationWeights(
-            model.layer,
-            weights=torch.ones(1),
-            neuron=True
+            model.layer, weights=torch.ones(1), neuron=True
         )
         assertArraysAlmostEqual(
             get_loss_value(model, loss),
-            [CHANNEL_ACTIVATION_0_LOSS,
-            CHANNEL_ACTIVATION_1_LOSS]
+            [CHANNEL_ACTIVATION_0_LOSS, CHANNEL_ACTIVATION_1_LOSS],
         )
 
 
@@ -195,48 +149,51 @@ class TestCompositeLoss(BaseTest):
     def test_negative(self) -> None:
         model = SimpleModel()
         loss = -opt_loss.ChannelActivation(model.layer, 0)
-        self.assertAlmostEqual(
-            get_loss_value(model, loss),
-            -CHANNEL_ACTIVATION_0_LOSS
-        )
+        self.assertAlmostEqual(get_loss_value(model, loss), -CHANNEL_ACTIVATION_0_LOSS)
+
     def test_addition(self) -> None:
         model = SimpleModel()
-        loss = opt_loss.ChannelActivation(model.layer, 0) + \
-            opt_loss.ChannelActivation(model.layer, 1) + 1
+        loss = (
+            opt_loss.ChannelActivation(model.layer, 0)
+            + opt_loss.ChannelActivation(model.layer, 1)
+            + 1
+        )
         self.assertAlmostEqual(
             get_loss_value(model, loss),
-            CHANNEL_ACTIVATION_0_LOSS + \
-            CHANNEL_ACTIVATION_1_LOSS + 1
+            CHANNEL_ACTIVATION_0_LOSS + CHANNEL_ACTIVATION_1_LOSS + 1,
         )
+
     def test_subtraction(self) -> None:
         model = SimpleModel()
-        loss = opt_loss.ChannelActivation(model.layer, 0) - \
-            opt_loss.ChannelActivation(model.layer, 1) - 1
+        loss = (
+            opt_loss.ChannelActivation(model.layer, 0)
+            - opt_loss.ChannelActivation(model.layer, 1)
+            - 1
+        )
         self.assertAlmostEqual(
             get_loss_value(model, loss),
-            CHANNEL_ACTIVATION_0_LOSS - \
-            CHANNEL_ACTIVATION_1_LOSS - 1
+            CHANNEL_ACTIVATION_0_LOSS - CHANNEL_ACTIVATION_1_LOSS - 1,
         )
+
     def test_multiplication(self) -> None:
         model = SimpleModel()
         loss = opt_loss.ChannelActivation(model.layer, 0) * 10
         self.assertAlmostEqual(
-            get_loss_value(model, loss),
-            CHANNEL_ACTIVATION_0_LOSS * 10
+            get_loss_value(model, loss), CHANNEL_ACTIVATION_0_LOSS * 10
         )
+
     def test_division(self) -> None:
         model = SimpleModel()
         loss = opt_loss.ChannelActivation(model.layer, 0) / 10
         self.assertAlmostEqual(
-            get_loss_value(model, loss),
-            CHANNEL_ACTIVATION_0_LOSS / 10
+            get_loss_value(model, loss), CHANNEL_ACTIVATION_0_LOSS / 10
         )
+
     def test_pow(self) -> None:
         model = SimpleModel()
         loss = opt_loss.ChannelActivation(model.layer, 0) ** 2
         self.assertAlmostEqual(
-            get_loss_value(model, loss),
-            CHANNEL_ACTIVATION_0_LOSS ** 2
+            get_loss_value(model, loss), CHANNEL_ACTIVATION_0_LOSS ** 2
         )
 
 
--- tests/optim/core/test_objectives.py
+++ tests/optim/core/test_objectives.py
@@ -1,21 +1,18 @@
 #!/usr/bin/env python3
 import unittest
-from typing import cast
-from contextlib import suppress
 
 import numpy as np
-import torch
 import torch.nn as nn
 
-from captum.optim._core.output_hook import AbortForwardException, ModuleOutputsHook
 import captum.optim as opt
-from tests.helpers.basic import BaseTest, assertArraysAlmostEqual
+from tests.helpers.basic import BaseTest
 
 
 class SimpleModel(nn.Module):
     def __init__(self):
         super(SimpleModel, self).__init__()
-        self.layer = nn.Conv2d(3, 2, 1)
+        self.layer = nn.Conv2d(3, 1, 1)
+
     def forward(self, x):
         return self.layer(x)
 
@@ -25,7 +22,10 @@ class TestInputOptimization(BaseTest):
         model = SimpleModel()
         loss_fn = opt.loss.ChannelActivation(model.layer, 0)
         obj = opt.InputOptimization(model, loss_function=loss_fn)
-        history = obj.optimize(opt.objectives.n_steps(128))
+        history = obj.optimize(opt.objectives.n_steps(5))
+        history = np.mean(np.mean(history, axis=2), axis=2)
+        for i, j in zip(history[:-1], history[1:]):
+            assert i < j
 
 
 if __name__ == "__main__":
