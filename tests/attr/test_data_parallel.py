3d9454917e4e6054693bc27940ab8643a8c2fd7f
ProGamerGov <ProGamerGov@users.noreply.github.com>
2021-03-14 12:18:05 -0600

    Fix Distributed Data Parallel tests failing in torch 1.8
    Fix Distributed Data Parallel tests failing in torch 1.8: https://github.com/pytorch/captum/pull/631

--- tests/attr/test_data_parallel.py
+++ tests/attr/test_data_parallel.py
@@ -156,8 +156,9 @@ class DataParallelMeta(type):
 
                 model_1, model_2 = (
                     cuda_model,
-                    torch.nn.parallel.DistributedDataParallel(cuda_model),
-                )
+                    torch.nn.parallel.DistributedDataParallel(
+                        cuda_model, device_ids=[0], output_device=0
+                    ),
                 args_1, args_2 = cuda_args, cuda_args
             else:
                 raise AssertionError("DataParallel compare mode type is not valid.")
