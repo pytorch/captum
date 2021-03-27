bb7f466c3f3f27956dd3c64cdce4dcde16a52c78
SK <limsweekiat@gmail.com>
2021-03-27 12:52:30 +0800

    Lint fix

--- captum/optim/models/__init__.py
+++ captum/optim/models/__init__.py
@@ -1,2 +1,5 @@
-from ._common import collect_activations, get_model_layers, replace_layers, RedirectedReluLayer  # noqa: F401
+from ._common import RedirectedReluLayer  # noqa: F401
+from ._common import collect_activations  # noqa: F401
+from ._common import get_model_layers  # noqa: F401
+from ._common import replace_layers  # noqa: F401
 from ._image.inception_v1 import googlenet  # noqa: F401
