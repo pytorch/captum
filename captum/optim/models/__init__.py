d7e6ef2c6529c6d8cd2a6a487998921a55cfd4f2
SK <limsweekiat@gmail.com>
2021-03-29 23:39:44 +0800

    Expose skip_layers in optim.models

--- captum/optim/models/__init__.py
+++ captum/optim/models/__init__.py
@@ -2,5 +2,6 @@ from ._common import RedirectedReluLayer  # noqa: F401
 from ._common import collect_activations  # noqa: F401
 from ._common import get_model_layers  # noqa: F401
 from ._common import replace_layers  # noqa: F401
+from ._common import skip_layers  # noqa: F401
 from ._image.inception_v1 import InceptionV1  # noqa: F401
 from ._image.inception_v1 import googlenet  # noqa: F401
