322f88217ec37ba93c16e92a06befd7e7f2cf1b1
SK <limsweekiat@gmail.com>
2021-03-30 10:59:38 +0800

    Refactor __init__ files

--- captum/optim/__init__.py
+++ captum/optim/__init__.py
@@ -1,11 +1,11 @@
 """optim submodule."""
 
-from captum.optim._core import loss  # noqa: F401
-from captum.optim._core import optimization  # noqa: F401
+from captum.optim._core import loss, optimization  # noqa: F401
 from captum.optim._core.optimization import InputOptimization  # noqa: F401
-from captum.optim._param.image import images  # noqa: F401
-from captum.optim._param.image import transform  # noqa: F401
+from captum.optim._param.image import images, transform  # noqa: F401
 from captum.optim._param.image.images import ImageTensor  # noqa: F401
 from captum.optim._utils import circuits, reducer  # noqa: F401
-from captum.optim._utils.image.common import nchannels_to_rgb  # noqa: F401
-from captum.optim._utils.image.common import weights_to_heatmap_2d  # noqa: F401
+from captum.optim._utils.image.common import (  # noqa: F401
+    nchannels_to_rgb,
+    weights_to_heatmap_2d,
+)
--- captum/optim/models/__init__.py
+++ captum/optim/models/__init__.py
@@ -1,7 +1,8 @@
-from ._common import RedirectedReluLayer  # noqa: F401
-from ._common import collect_activations  # noqa: F401
-from ._common import get_model_layers  # noqa: F401
-from ._common import replace_layers  # noqa: F401
-from ._common import skip_layers  # noqa: F401
-from ._image.inception_v1 import InceptionV1  # noqa: F401
-from ._image.inception_v1 import googlenet  # noqa: F401
+from ._common import (  # noqa: F401
+    RedirectedReluLayer,
+    collect_activations,
+    get_model_layers,
+    replace_layers,
+    skip_layers,
+)
+from ._image.inception_v1 import InceptionV1, googlenet  # noqa: F401
