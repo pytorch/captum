fa44939d5d4ca0549623ebb93ad3cf65354fdff0
SK <limsweekiat@gmail.com>
2021-04-01 17:21:06 +0800

    Lint fix

--- captum/optim/models/__init__.py
+++ captum/optim/models/__init__.py
@@ -5,12 +5,11 @@ from ._common import (  # noqa: F401
     replace_layers,
     skip_layers,
 )
+from ._image.inception5h_classes import INCEPTION5H_CLASSES  # noqa: F401
 from ._image.inception_v1 import InceptionV1, googlenet  # noqa: F401
-from ._image.inception5h_classes import INCEPTION5H_CLASSES # noqa: F401
-
 
 __all__ = [
-	"RedirectedReluLayer",
+    "RedirectedReluLayer",
     "collect_activations",
     "get_model_layers",
     "replace_layers",
--- tests/optim/param/test_transforms.py
+++ tests/optim/param/test_transforms.py
@@ -657,15 +657,15 @@ class TestAlphaChannelLoss(BaseTest):
         raise unittest.SkipTest(
             "Skipping AlphaChannelLoss test until function is ready."
         )
-        crop_size = [160, 160]
-        scale = [0.6, 0.7, 0.8, 0.9, 1.0, 1.1]
+        # crop_size = [160, 160]
+        # scale = [0.6, 0.7, 0.8, 0.9, 1.0, 1.1]
 
-        alpha_loss_transform = transforms.AlphaChannelLoss(
-            scale=scale, crop_size=crop_size
-        )
-        x = torch.randn(1, 4, 224, 224)
+        # alpha_loss_transform = transforms.AlphaChannelLoss(
+        #     scale=scale, crop_size=crop_size
+        # )
+        # x = torch.randn(1, 4, 224, 224)
 
-        x_out = alpha_loss_transform(x)
+        # x_out = alpha_loss_transform(x)
 
-        assertTensorAlmostEqual(self, x_out, x, 0)
-        self.assertNotEqual(alpha_loss_transforms.loss, 0)
+        # assertTensorAlmostEqual(self, x_out, x, 0)
+        # self.assertNotEqual(alpha_loss_transforms.loss, 0)
