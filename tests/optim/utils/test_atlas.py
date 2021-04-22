a6a34924f1272913cb7c736190cc59cac788dc63
ProGamerGov <ProGamerGov@users.noreply.github.com>
2021-04-22 15:46:47 -0600

    Add CUDA tests for atlas functions

--- tests/optim/utils/test_atlas.py
+++ tests/optim/utils/test_atlas.py
@@ -83,12 +83,42 @@ class TestNormalizeGrid(BaseTest):
 
         assertTensorAlmostEqual(self, xy_grid, xy_grid_expected, 0.001)
 
+    def test_normalize_grid_cuda(self) -> None:
+        if not torch.cuda.is_available():
+            raise unittest.SkipTest(
+                "Skipping normalize grid CUDA test due to not supporting CUDA."
+            )
+        if torch.__version__ < "1.7.0":
+            raise unittest.SkipTest(
+                "Skipping normalize grid CUDA test due to insufficient Torch version."
+            )
+        xy_grid = torch.arange(0, 2 * 3 * 3).view(3 * 3, 2).float().cuda()
+
+        xy_grid = atlas.normalize_grid(xy_grid)
+
+        xy_grid_expected = torch.tensor(
+            [
+                [0.0000, 0.0000],
+                [0.1250, 0.1250],
+                [0.2500, 0.2500],
+                [0.3750, 0.3750],
+                [0.5000, 0.5000],
+                [0.6250, 0.6250],
+                [0.7500, 0.7500],
+                [0.8750, 0.8750],
+                [1.0000, 1.0000],
+            ]
+        )
+
+        self.assertTrue(xy_grid.is_cuda)
+        assertTensorAlmostEqual(self, xy_grid.cpu(), xy_grid_expected)
+
 
 class TestCalcGridIndices(BaseTest):
     def test_calc_grid_indices(self) -> None:
         if torch.__version__ < "1.7.0":
             raise unittest.SkipTest(
-                "Skipping grid indices test due to insufficient Torch version."
+                "Skipping calc grid indices test due to insufficient Torch version."
             )
         xy_grid = torch.arange(0, 2 * 3 * 3).view(3 * 3, 2).float()
         xy_grid = atlas.normalize_grid(xy_grid)
@@ -106,7 +136,8 @@ class TestCalcGridIndices(BaseTest):
     def test_calc_grid_indices_extent(self) -> None:
         if torch.__version__ < "1.7.0":
             raise unittest.SkipTest(
-                "Skipping grid indices extent test due to insufficient Torch version."
+                "Skipping calc grid indices extent test due to insufficient Torch"
+                + " version."
             )
         xy_grid = torch.arange(0, 2 * 5 * 5).view(5 * 5, 2).float()
         xy_grid = atlas.normalize_grid(xy_grid)
@@ -114,6 +145,30 @@ class TestCalcGridIndices(BaseTest):
             xy_grid, grid_size=(1, 1), x_extent=(1.0, 2.0), y_extent=(1.0, 2.0)
         )
         assertTensorAlmostEqual(self, indices[0][0], torch.tensor([24]), 0)
+
+    def test_calc_grid_indices_cuda(self) -> None:
+        if not torch.cuda.is_available():
+            raise unittest.SkipTest(
+                "Skipping calc grid indices CUDA test due to not supporting CUDA."
+            )
+        if torch.__version__ < "1.7.0":
+            raise unittest.SkipTest(
+                "Skipping calc grid indices CUDA test due to insufficient Torch"
+                + " version."
+            )
+        xy_grid = torch.arange(0, 2 * 3 * 3).view(3 * 3, 2).float().cuda()
+        xy_grid = atlas.normalize_grid(xy_grid)
+        indices = atlas.calc_grid_indices(xy_grid, grid_size=(2, 2))
+
+        expected_indices = [
+            [torch.tensor([0, 1, 2, 3, 4]), torch.tensor([4])],
+            [torch.tensor([4]), torch.tensor([4, 5, 6, 7, 8])],
+        ]
+
+        for list1, list2 in zip(indices, expected_indices):
+            for t1, t2 in zip(list1, list2):
+                self.assertTrue(t1.is_cuda)
+                assertTensorAlmostEqual(self, t1.cpu(), t2)
 
 
 class TestExtractGridVectors(BaseTest):
@@ -142,7 +197,7 @@ class TestExtractGridVectors(BaseTest):
         if torch.__version__ < "1.7.0":
             raise unittest.SkipTest(
                 "Skipping extract grid vectors assertion test due to insufficient"
-                + "Torch version."
+                + " Torch version."
             )
 
         grid_size = (2, 2)
@@ -155,6 +210,33 @@ class TestExtractGridVectors(BaseTest):
             vecs, vec_coords = atlas.extract_grid_vectors(
                 grid_indices, raw_activ, grid_size=grid_size, min_density=50
             )
+
+    def test_extract_grid_vectors_cuda(self) -> None:
+        if not torch.cuda.is_available():
+            raise unittest.SkipTest(
+                "Skipping extract grid vectors CUDA test due to not supporting CUDA."
+            )
+        if torch.__version__ < "1.7.0":
+            raise unittest.SkipTest(
+                "Skipping extract grid vectors CUDA test due to insufficient Torch"
+                + " version."
+            )
+        grid_size = (2, 2)
+        raw_activ = torch.arange(0, 4 * 3 * 3).view(3 * 3, 4).float().cuda()
+        xy_grid = torch.arange(0, 2 * 3 * 3).view(3 * 3, 2).float().cuda()
+        xy_grid = atlas.normalize_grid(xy_grid)
+        grid_indices = atlas.calc_grid_indices(xy_grid, grid_size=grid_size)
+
+        vecs, vec_coords = atlas.extract_grid_vectors(
+            grid_indices, raw_activ, grid_size=grid_size, min_density=2
+        )
+
+        expected_vecs = torch.tensor([[8.0, 9.0, 10.0, 11.0], [24.0, 25.0, 26.0, 27.0]])
+        expected_coords = [(0, 0, 5), (1, 1, 5)]
+
+        self.assertTrue(vecs.is_cuda)
+        assertTensorAlmostEqual(self, vecs.cpu(), expected_vecs)
+        self.assertEqual(vec_coords, expected_coords)
 
 
 class TestCreateAtlasVectors(BaseTest):
@@ -196,6 +278,29 @@ class TestCreateAtlasVectors(BaseTest):
         assertTensorAlmostEqual(self, vecs, expected_vecs)
         self.assertEqual(vec_coords, expected_coords)
 
+    def test_create_atlas_vectors_cuda(self) -> None:
+        if not torch.cuda.is_available():
+            raise unittest.SkipTest(
+                "Skipping create atlas vectors CUDA test due to not supporting CUDA."
+            )
+        if torch.__version__ < "1.7.0":
+            raise unittest.SkipTest(
+                "Skipping create atlas vectors CUDA test due to insufficient Torch"
+                + " version."
+            )
+        raw_activ = torch.arange(0, 4 * 3 * 3).view(3 * 3, 4).float().cuda()
+        xy_grid = torch.arange(0, 2 * 3 * 3).view(3 * 3, 2).float().cuda()
+        vecs, vec_coords = atlas.create_atlas_vectors(
+            xy_grid, raw_activ, grid_size=(2, 2), min_density=2, normalize=True
+        )
+
+        expected_vecs = torch.tensor([[8.0, 9.0, 10.0, 11.0], [24.0, 25.0, 26.0, 27.0]])
+        expected_coords = [(0, 0, 5), (1, 1, 5)]
+
+        self.assertTrue(vecs.is_cuda)
+        assertTensorAlmostEqual(self, vecs.cpu(), expected_vecs)
+        self.assertEqual(vec_coords, expected_coords)
+
 
 class TestCreateAtlas(BaseTest):
     def test_create_atlas_square_grid_size(self) -> None:
@@ -219,7 +324,7 @@ class TestCreateAtlas(BaseTest):
         if torch.__version__ < "1.7.0":
             raise unittest.SkipTest(
                 "Skipping create atlas canvas tensor stack test due to insufficient"
-                + "Torch version."
+                + " Torch version."
             )
         grid_size = (2, 2)
         img_stack = torch.stack([torch.zeros(3, 4, 4)] * 2, dim=0)
@@ -270,3 +375,26 @@ class TestCreateAtlas(BaseTest):
             [torch.vstack((c_pattern, c_pattern.flip(1)))] * 3, 0
         ).unsqueeze(0)
         assertTensorAlmostEqual(self, atlas_canvas, expected_canvas, 0)
+
+    def test_create_atlas_square_grid_size_cuda(self) -> None:
+        if not torch.cuda.is_available():
+            raise unittest.SkipTest(
+                "Skipping create atlas CUDA test due to not supporting CUDA."
+            )
+        if torch.__version__ < "1.7.0":
+            raise unittest.SkipTest(
+                "Skipping create_atlas canvas CUDA due to insufficient Torch version."
+            )
+        grid_size = (2, 2)
+        img_list = [torch.zeros(1, 3, 4, 4).cuda()] * 2
+        vec_coords = [(0, 0), (1, 1)]
+
+        atlas_canvas = atlas.create_atlas(img_list, vec_coords, grid_size=grid_size)
+
+        c_pattern = torch.hstack((torch.ones(4, 4), torch.zeros(4, 4)))
+        expected_canvas = torch.stack(
+            [torch.vstack((c_pattern, c_pattern.flip(1)))] * 3, 0
+        ).unsqueeze(0)
+
+        self.assertTrue(atlas_canvas.is_cuda)
+        assertTensorAlmostEqual(self, atlas_canvas.cpu(), expected_canvas, 0)
