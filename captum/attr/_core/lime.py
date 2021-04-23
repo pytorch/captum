Fix KernelShap Overflow Issues (#649)
Summary:
This ensures that Lime / Kernel Shap interpretable model inputs, targets, and similarity scores are floats, which avoids sporadic int overflow issues occurring in Kernel Shap.

Pull Request resolved: https://github.com/pytorch/captum/pull/649

Reviewed By: miguelmartin75

Differential Revision: D27604382

Pulled By: vivekmig

fbshipit-source-id: 7049de0a0ba488bcb7619cd917e825fa07fe77da

captum/attr/_core/lime.py
@@ -511,17 +511,17 @@ class LimeBase(PerturbationAttribution):
             if show_progress:
                 attr_progress.close()
 
-            combined_interp_inps = torch.cat(interpretable_inps)
+            combined_interp_inps = torch.cat(interpretable_inps).double()
             combined_outputs = (
                 torch.cat(outputs)
                 if len(outputs[0].shape) > 0
                 else torch.stack(outputs)
-            )
+            ).double()
             combined_sim = (
                 torch.cat(similarities)
                 if len(similarities[0].shape) > 0
                 else torch.stack(similarities)
-            )
+            ).double()
             dataset = TensorDataset(
                 combined_interp_inps, combined_outputs, combined_sim
             )