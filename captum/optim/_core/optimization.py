d31d60d605b3d532288f668feb1e52f68867d058
ProGamerGov <ProGamerGov@users.noreply.github.com>
2021-04-26 15:35:51 -0600

    Ensure history has no gradient

--- captum/optim/_core/optimization.py
+++ captum/optim/_core/optimization.py
@@ -138,7 +138,7 @@ class InputOptimization(Objective, Parameterized):
             while stop_criteria(step, self, history, optimizer):
                 optimizer.zero_grad()
                 loss_value = loss_summarize_fn(self.loss())
-                history.append(loss_value)
+                history.append(loss_value.clone().detach().cpu())
                 loss_value.backward()
                 optimizer.step()
                 step += 1
