685ced8f80b4ecff6fa69ee9540a6d8bc771fe0e
Gregor Richter <Gregor.Richter@umlaut.com>
2021-05-20 09:44:54 +0200

    Added additional lrp rules

--- captum/attr/_utils/lrp_rules.py
+++ captum/attr/_utils/lrp_rules.py
@@ -189,3 +189,426 @@ class IdentityRule(EpsilonRule):
             return self.relevance_output
 
         return _backward_hook_input
+
+
+class WSquaredRule(PropagationRule):
+    r"""
+    W^2-rule for relevance propagation, cf. https://arxiv.org/pdf/1512.02479.
+    Does not weight the relevance by the layers input.
+
+    Use for input layer.
+    """
+
+    def __init__(self) -> None:
+        self._input_shapes = None
+        self._denominator = None
+
+    def forward_hook(self, module: torch.nn.Module, inputs: Tuple[Tensor, ...], outputs: Tensor) -> None:
+        super().forward_hook(module, inputs, outputs)
+        self._compute_denominator(module, inputs)
+
+    def _compute_denominator(self, module: torch.nn.Module, inputs: Tuple[Tensor, ...]) -> None:
+        input_shapes = tuple(x.shape[1:] for x in inputs)
+
+        if input_shapes != self._input_shapes:
+            self._input_shapes = input_shapes
+            with torch.no_grad():
+                self._denominator = module.forward(
+                    *tuple(torch.ones(input_shape).unsqueeze(dim=0) for input_shape in self._input_shapes)
+                )
+                self._denominator += self.STABILITY_FACTOR
+
+    def _create_backward_hook_input(self, input_: Tensor, input_index: int) -> Callable[[Tensor], None]:
+        def _backward_hook_input(grad: Tensor) -> None:  # pylint: disable=unused-argument
+            pass
+
+        return _backward_hook_input
+
+    def _create_backward_hook_output(self, output: Tensor) -> Callable[[Tensor], Tensor]:
+        def _backward_hook_output(grad: Tensor) -> Tensor:
+            relevance = grad / torch.cat(tuple(self._denominator for _ in range(output.shape[0])))
+            return relevance
+
+        return _backward_hook_output
+
+    def _manipulate_weights(self, module: torch.nn.Module, inputs: Tuple[Tensor, ...], outputs: Tensor) -> None:
+        if hasattr(module, "bias"):
+            if module.bias is not None:
+                module.bias.data = torch.zeros_like(module.bias.data)
+        if hasattr(module, "weight"):
+            module.weight.data = module.weight.data ** 2
+
+
+class FlatRule(WSquaredRule):
+    r"""
+    Flat rule for relevance backpropagation. This rule weights the relevance
+    by the number of contributing input neurons.
+
+    Use for lower layers.
+    """
+
+    def _manipulate_weights(self, module: torch.nn.Module, inputs: Tuple[Tensor, ...], outputs: Tensor) -> None:
+        if hasattr(module, "bias"):
+            if module.bias is not None:
+                module.bias.data = torch.zeros_like(module.bias.data)  # pylint: disable=E1101
+        if hasattr(module, "weight"):
+            module.weight.data = torch.ones_like(module.weight.data)  # pylint: disable=E1101
+
+
+class AlphaBetaRule(PropagationRule):
+    r"""
+    Alpha Beta rule for relevance backpropagation. This rule weights
+    the positive and negative addends contributing to some pre-activation
+    asymmetrically via coefficients alpha and beta (alpha + beta = 1).
+
+    Use for lower layers.
+    """
+
+    def __init__(self, alpha: float = 1.0, set_bias_to_zero: bool = False) -> None:
+        r"""
+        Args:
+            alpha (float, optional): Alpha parameter of alpha beta rule.
+            Defaults to 1.
+
+            set_bias_to_zero (bool, optional): Parameter for setting bias to
+            zero in relevance computations.
+            Defaults to False.
+        """
+        self.alpha = alpha
+        self.beta = 1.0 - self.alpha
+        self.set_bias_to_zero = set_bias_to_zero
+
+        self._module_pos = None
+        self._module_neg = None
+        self._bias_contrib = None
+
+    def forward_hook(self, module: torch.nn.Module, inputs: Tuple[Tensor, ...], outputs: Tensor) -> None:
+        r"""Register backward hooks on input and output tensors of linear layers in the model."""
+        if not hasattr(module, "weight"):
+            raise RuntimeError(
+                f"AlphaBetaRule assigned to module without weights: {module}. "
+                + "This rule only supports modules with weight."
+            )
+        super().forward_hook(module, inputs, outputs)
+
+        self._create_signed_inputs(inputs)
+
+    def _create_backward_hook_input(
+        self, input_: Tensor, input_index: int
+    ) -> Callable[[Tensor,], Tensor]:
+        def _backward_hook_input(grad: Tensor) -> Tensor:
+            out = self.out[input_index]
+            return out
+
+        return _backward_hook_input
+
+    def _create_backward_hook_output(self, output: Tensor) -> Callable[[Tensor], Tensor]:
+        def _backward_hook_output(grad: Tensor) -> Tensor:
+            out = self._compute_signed_contributions(grad, self.inputs_pos, self.inputs_neg, +1)
+
+            if self.beta:
+                out_beta = tuple(
+                    self.beta * rel
+                    for rel in self._compute_signed_contributions(grad, self.inputs_neg, self.inputs_pos, -1)
+                )
+                out = tuple(self.alpha * x + y for x, y in zip(out, out_beta))
+
+            self.out = out
+
+        return _backward_hook_output
+
+    def _compute_signed_contributions(
+        self, grad: Tensor, mod_pos_in: Tuple[Tensor, ...], mod_neg_in: Tuple[Tensor, ...], sign: int
+    ) -> Tuple[Tensor, ...]:
+        r"""
+        if mod_pos_in is the positive part of the inputs and mod_neg_in the negative this computes the alpha part
+        if mod_pos_in is the negative part of the inputs and mod_neg_in the positive this computes the beta part
+        """
+        with torch.autograd.set_grad_enabled(True):
+            mod_pos_out = self._module_pos.forward(*mod_pos_in)
+            mod_neg_out = self._module_neg.forward(*mod_neg_in)
+
+            denominator = mod_pos_out + mod_neg_out
+            if not self.set_bias_to_zero and self._bias_contrib is not None:
+                if sign == 1:
+                    denominator += torch.cat(tuple(self._bias_contrib for _ in range(denominator.shape[0]))).clamp(
+                        min=0
+                    )
+                else:
+                    denominator += torch.cat(tuple(self._bias_contrib for _ in range(denominator.shape[0]))).clamp(
+                        max=0
+                    )
+
+            # this might be unneccessary, simply adding epsilon may be enough
+            denominator += sign * (torch.eq(denominator, 0.0)) * self.STABILITY_FACTOR
+
+            rescaled_relevance = grad / denominator
+
+            # getting contractions with transposed Jacobian
+            res_wp = torch.autograd.grad(outputs=mod_pos_out, inputs=mod_pos_in, grad_outputs=rescaled_relevance)
+            res_wm = torch.autograd.grad(outputs=mod_neg_out, inputs=mod_neg_in, grad_outputs=rescaled_relevance)
+
+            out = tuple(
+                (pos_in * jac_pos) + (neg_in * jac_neg)
+                for jac_pos, pos_in, jac_neg, neg_in in zip(res_wp, mod_pos_in, res_wm, mod_neg_in)
+            )
+
+        return out
+
+    def _create_signed_inputs(self, inputs: Tuple[Tensor, ...]) -> None:
+        self.inputs_pos = tuple(input_.data.clamp(min=0) for input_ in inputs)
+        self.inputs_neg = tuple(input_.data.clamp(max=0) for input_ in inputs)
+
+        for input_pos, input_neg in zip(self.inputs_pos, self.inputs_neg):
+            input_pos.requires_grad_(True)
+            input_neg.requires_grad_(True)
+
+    def _separate_weights_by_sign(self, module: torch.nn.Module) -> None:
+        if self._module_neg is None:
+            self._module_neg = copy.deepcopy(module)
+            self._module_neg.weight.data = self._module_neg.weight.data.clamp(max=0.0)
+        self._module_pos = module
+        self._module_pos.weight.data = self._module_pos.weight.data.clamp(min=0.0)
+
+    def _manipulate_weights(self, module: torch.nn.Module, inputs: Tuple[Tensor, ...], outputs: Tensor) -> None:
+        if hasattr(module, "bias"):
+            if module.bias is not None:
+                if not self.set_bias_to_zero and self._bias_contrib is None:
+                    with torch.no_grad():
+                        self._bias_contrib = module.forward(
+                            *(torch.zeros(input_.shape[1:]).unsqueeze(dim=0) for input_ in inputs)
+                        )
+                module.bias.data = torch.zeros_like(module.bias.data)
+
+        self._separate_weights_by_sign(module)
+
+
+class AlphaBetaRuleV2(AlphaBetaRule):
+    r"""
+    AlphaBetaRule with decreased execution times but increased memory consumption.
+    """
+
+    def _create_backward_hook_input(
+        self, input_: Tensor, input_index: int
+    ) -> Callable[[Tensor,], Tensor]:
+        def _backward_hook_input(grad: Tensor) -> Tensor:
+            out = input_ * grad
+            out += self.out[input_index]
+            return out
+
+        return _backward_hook_input
+
+    def _create_backward_hook_output(self, output: Tensor) -> Callable[[Tensor], Tensor]:
+        def _backward_hook_output(grad: Tensor) -> Tensor:
+            pos_denominator = self._compute_pos_denominator(output)
+            if self.beta:
+                neg_denominator = self.og_outputs[0] - pos_denominator
+                if self.set_bias_to_zero and self._bias_contrib is not None:
+                    neg_denominator -= torch.cat(tuple(self._bias_contrib for _ in range(neg_denominator.shape[0])))
+
+                neg_denominator -= self.STABILITY_FACTOR
+                beta_rel = (self.beta * grad) / neg_denominator
+
+                res_wp_beta = torch.autograd.grad(
+                    inputs=self.inputs_pos, outputs=self.wp_xp, grad_outputs=beta_rel, retain_graph=False
+                )
+                res_wm_beta = torch.autograd.grad(
+                    inputs=self.inputs_neg, outputs=self.wm_xm, grad_outputs=beta_rel, retain_graph=True
+                )
+
+                out_beta = list(
+                    xm * wp_beta + xp * wm_beta
+                    for xm, wp_beta, xp, wm_beta in zip(self.inputs_neg, res_wp_beta, self.inputs_pos, res_wm_beta)
+                )
+
+            pos_denominator += self.STABILITY_FACTOR
+            alpha_rel = (self.alpha * grad) / pos_denominator
+
+            res_wm_alpha = torch.autograd.grad(
+                inputs=self.inputs_neg, outputs=self.wm_xm, grad_outputs=alpha_rel, retain_graph=False
+            )
+
+            out_alpha = list(xm * wm_alpha for xm, wm_alpha in zip(self.inputs_neg, res_wm_alpha))
+
+            if self.beta:
+                out_alpha = list(x + y for x, y in zip(out_alpha, out_beta))
+
+            self.out = out_alpha
+
+            return alpha_rel
+
+        return _backward_hook_output
+
+    def _compute_pos_denominator(self, output: Tensor) -> Tensor:
+        if not self.beta:
+            with torch.no_grad():
+                pos_denominator = output
+            with torch.autograd.set_grad_enabled(True):
+                self.wm_xm = self._module_neg.forward(*self.inputs_neg)
+
+            pos_denominator += self.wm_xm
+        else:
+            with torch.autograd.set_grad_enabled(True):
+                self.wp_xp = self._module_pos.forward(*self.inputs_pos)
+                self.wm_xm = self._module_neg.forward(*self.inputs_neg)
+
+            pos_denominator = self.wp_xp + self.wm_xm
+
+        if not self.set_bias_to_zero and self._bias_contrib is not None:
+            pos_denominator += torch.cat(tuple(self._bias_contrib for _ in range(pos_denominator.shape[0]))).clamp(
+                min=0
+            )
+
+        return pos_denominator.detach()
+
+    def forward_hook_weights(
+        self,
+        module: torch.nn.Module,
+        inputs: Tuple[Tensor, ...],
+        outputs: Tuple[Tensor, ...],
+    ) -> None:
+        super().forward_hook_weights(module, inputs, outputs)
+        self.og_outputs = outputs
+
+    def _manipulate_weights(self, module: torch.nn.Module, inputs: Tuple[Tensor, ...], outputs: Tensor) -> None:
+        if hasattr(module, "bias"):
+            if module.bias is not None:
+                if self._bias_contrib is None:
+                    with torch.no_grad():
+                        self._bias_contrib = module.forward(
+                            *(torch.zeros(input_.shape[1:]).unsqueeze(dim=0) for input_ in inputs)
+                        )
+                module.bias.data = torch.zeros_like(module.bias.data)
+
+        self._separate_weights_by_sign(module)
+
+    def forward_pre_hook_activations(self, module: torch.nn.Module, inputs: Tuple[Tensor, ...]) -> Tuple[Tensor, ...]:
+        for input_, activation in zip(inputs, module.activations):
+            input_.data = activation.clamp(min=0)
+        return inputs
+
+
+class ZBoundRule(PropagationRule):
+    def __init__(
+        self,
+        lower_bound: Union[int, float] = -1.0,
+        upper_bound: Union[int, float] = 1.0,
+        set_bias_to_zero: bool = True,
+    ) -> None:
+        r"""
+        If lower_bound is a float, this will be used for every input feature.
+        If lower bound is a tuple, each entry will be used for all features of
+        the corresponding input.
+        """
+        self.upper_bound = upper_bound
+        self.lower_bound = lower_bound
+        self.set_bias_to_zero = set_bias_to_zero
+
+        self._lower_bound_tensor = None
+        self._upper_bound_tensor = None
+
+        self._module_pos = None
+        self._module_neg = None
+        self._bias_contrib = None
+
+        self._denominator_bound_contribution = None
+        self._input_shapes = None
+
+    def forward_hook(self, module: torch.nn.Module, inputs: Tuple[Tensor, ...], outputs: Tensor) -> None:
+        r"""
+        Register backward hooks on input and output
+        tensors of linear layers in the model.
+        """
+        if not hasattr(module, "weight"):
+            raise RuntimeError(
+                f"ZBoundRule assigned to module without weights: {module}. "
+                + "This rule only supports modules with weight."
+            )
+        super().forward_hook(module, inputs, outputs)
+
+        self._weight_contraction_with_bounds(inputs)
+
+    def _create_backward_hook_input(
+        self, input_: Tensor, input_index: int
+    ) -> Callable[[Tensor,], Tensor]:
+        def _backward_hook_input(grad: Tensor) -> Tensor:
+            relevance = (input_ - self._lower_bound_tensor[input_index]) * grad
+            relevance += (input_ - self._upper_bound_tensor[input_index]) * self.res_wm[input_index]
+
+            return relevance
+
+        return _backward_hook_input
+
+    def _create_backward_hook_output(
+        self, output: Tensor
+    ) -> Callable[[Tensor,], Tensor]:
+        def _backward_hook_output(grad: Tensor) -> Tensor:
+
+            denominator = self.og_outputs[0] - self._denominator_bound_contribution
+            if self.set_bias_to_zero and self._bias_contrib is not None:
+                denominator -= torch.cat(tuple(self._bias_contrib for _ in range(output.shape[0])))
+            denominator += self.STABILITY_FACTOR
+
+            rescaled_relevance = grad / denominator
+            self.res_wm = torch.autograd.grad(
+                outputs=self.upper_bound_contrib,
+                inputs=self._upper_bound_tensor,
+                grad_outputs=rescaled_relevance,
+                retain_graph=True,
+            )
+
+            return rescaled_relevance
+
+        return _backward_hook_output
+
+    def _weight_contraction_with_bounds(self, inputs: Tuple[Tensor, ...]) -> None:
+        r"""
+        Computes the l w^+ + h w^- term for the denominator.
+        """
+        input_shapes = tuple(x.shape for x in inputs)
+
+        if input_shapes != self._input_shapes:
+            self._input_shapes = input_shapes
+
+            with torch.autograd.set_grad_enabled(True):
+                self._upper_bound_tensor = tuple(
+                    torch.full(input_shape, self.upper_bound, requires_grad=True) for input_shape in self._input_shapes
+                )
+                self.upper_bound_contrib = self._module_neg.forward(*self._upper_bound_tensor)
+
+            with torch.no_grad():
+                self._lower_bound_tensor = tuple(
+                    torch.full(input_shape, self.lower_bound) for input_shape in self._input_shapes
+                )
+                self._denominator_bound_contribution = (
+                    self._module_pos.forward(*self._lower_bound_tensor) + self.upper_bound_contrib
+                )
+
+    def forward_hook_weights(
+        self,
+        module: torch.nn.Module,
+        inputs: Tuple[Tensor, ...],
+        outputs: Tuple[Tensor, ...],
+    ) -> None:
+        super().forward_hook_weights(module, inputs, outputs)
+        self.og_outputs = outputs.detach()
+
+    def _separate_weights_by_sign(self, module: torch.nn.Module) -> None:
+        if hasattr(module, "weight"):
+            if self._module_neg is None:
+                self._module_neg = copy.deepcopy(module)
+                self._module_neg.weight.data = self._module_neg.weight.data.clamp(max=0.0)
+            self._module_pos = module
+            self._module_pos.weight.data = self._module_pos.weight.data.clamp(min=0.0)
+
+    def _manipulate_weights(self, module: torch.nn.Module, inputs: Tuple[Tensor, ...], outputs: Tensor) -> None:
+        if hasattr(module, "bias"):
+            if module.bias is not None:
+                if self.set_bias_to_zero and self._bias_contrib is None:
+                    with torch.no_grad():
+                        self._bias_contrib = module.forward(
+                            *(torch.zeros(input_.shape[1:]).unsqueeze(dim=0) for input_ in inputs)
+                        )
+                module.bias.data = torch.zeros_like(module.bias.data)
+
+        self._separate_weights_by_sign(module)
