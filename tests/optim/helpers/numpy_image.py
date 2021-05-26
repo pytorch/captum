9ca0f9bbb66f01cb872012693db9d5d39064a49a
ProGamerGov <ProGamerGov@users.noreply.github.com>
2021-05-25 18:38:52 -0600

    Change NumPy rfft2d_freqs to match PyTorch version

--- tests/optim/helpers/numpy_image.py
+++ tests/optim/helpers/numpy_image.py
@@ -51,13 +51,10 @@ class FFTImage:
     def rfft2d_freqs(height: int, width: int) -> np.ndarray:
         """Computes 2D spectrum frequencies."""
         fy = np.fft.fftfreq(height)[:, None]
-        # on odd input dimensions we need to keep one additional frequency
-        wadd = 2 if width % 2 == 1 else 1
-        fx = np.fft.fftfreq(width)[: width // 2 + wadd]
+        fx = np.fft.fftfreq(width)[: width // 2 + 1]
         return np.sqrt((fx * fx) + (fy * fy))
 
     def forward(self) -> np.ndarray:
-        h, w = self.size
         scaled_spectrum = self.fourier_coeffs * self.spectrum_scale
         scaled_spectrum = scaled_spectrum.astype(complex)
         output = np.fft.irfftn(scaled_spectrum, s=self.size)
