61346de0eb0deb1fdb300e9d751d6f4a8dd5b988
Vivek Miglani <vivekm@fb.com>
2021-07-01 10:03:08 -0700

    Adding Minimal Perturbation Metric (#701)
    Summary:
    Pull Request resolved: https://github.com/pytorch/captum/pull/701
    
    Add Minimal Perturbation metric as a robustness metrics to work with Captum perturbations as well as arbitrary transformations such as torchvision transforms.
    
    Reviewed By: aobo-y
    
    Differential Revision: D29123001
    
    fbshipit-source-id: 3428f773b6e6f668104ecffd794a711c4cb86247

--- captum/robust/__init__.py
+++ captum/robust/__init__.py
@@ -2,5 +2,8 @@
 
 from captum.robust._core.fgsm import FGSM  # noqa
 from captum.robust._core.metrics.attack_comparator import AttackComparator  # noqa
+from captum.robust._core.metrics.min_param_perturbation import (  # noqa
+    MinParamPerturbation,
+)
 from captum.robust._core.perturbation import Perturbation  # noqa
 from captum.robust._core.pgd import PGD  # noqa
