4acb14a0e3802e63153752d1076535df66cf4354
Narine Kokhlikyan <narine@fb.com>
2021-07-21 14:38:18 -0700

    fix mypy issue

--- captum/_utils/models/linear_model/train.py
+++ captum/_utils/models/linear_model/train.py
@@ -349,8 +349,8 @@ def sklearn_train_linear_model(
 
     num_outputs = sklearn_model.coef_.shape[0] if sklearn_model.coef_.ndim > 1 else 1
     weight_values = torch.FloatTensor(sklearn_model.coef_).to(device)  # type: ignore
-    bias_values = torch.FloatTensor([sklearn_model.intercept_]).to(  # type: ignore
-        device
+    bias_values = torch.FloatTensor([sklearn_model.intercept_]).to(
+        device  # type: ignore
     )
     model._construct_model_params(
         norm_type=None,
