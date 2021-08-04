d77972e52e39aafef5d0b5a230a703f346cf117e
ProGamerGov <ProGamerGov@users.noreply.github.com>
2021-08-04 17:05:57 -0600

    Only commit atlas setup specific changes

--- captum/optim/__init__.py
+++ captum/optim/__init__.py
@@ -5,7 +5,7 @@ from captum.optim._core import loss, optimization  # noqa: F401
 from captum.optim._core.optimization import InputOptimization  # noqa: F401
 from captum.optim._param.image import images, transforms  # noqa: F401
 from captum.optim._param.image.images import ImageTensor  # noqa: F401
-from captum.optim._utils import atlas, circuits, reducer  # noqa: F401
+from captum.optim._utils import circuits, reducer  # noqa: F401
 from captum.optim._utils.image.common import (  # noqa: F401
     nchannels_to_rgb,
     save_tensor_as_image,
