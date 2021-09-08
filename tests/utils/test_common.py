74491c02d71a46bbeaecc0e82e13e8ce5abe65a1
Oliver Aobo Yang <aoboyang@fb.com>
2021-09-08 16:19:02 -0700

    fix test_common format

--- tests/utils/test_common.py
+++ tests/utils/test_common.py
@@ -13,7 +13,7 @@ class Test(BaseTest):
         assert safe_div(num, 2) == 2.0
         assert safe_div(num, 0, 2) == 2.0
         assert safe_div(num, 2.0) == 2.0
-        assert safe_div(num, 0., 2.0) == 2.0
+        assert safe_div(num, 0.0, 2.0) == 2.0
 
     def test_safe_div_tensor_denom(self):
         num = torch.tensor([4.0, 6.0])
