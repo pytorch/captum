82ab88d9ae8df82e91e9f5d5b1a25f7d4083ee57
ProGamerGov <ProGamerGov@users.noreply.github.com>
2022-01-26 20:43:26 -0700

    Optim-wip: Add main Activation Atlas tutorial & functions (#782)
    * Add main Activation Atlas classes & functions
    
    * Add main Activation Atlas tutorial notebook
    
    * Remove unsued import
    
    * Changes based on feedback
    
    * Underscore rotation class functions
    * Fix & improve documentation.
    * Revert weight heatmap variable name changes.
    * Correct rotation transform bug.
    
    * Revert ufmt change as it causes isort to fail
    
    * Improve documentation of `AngledNeuronDirection` & atlas functions
    
    * Improve atlas related documentation
    
    * Move atlas.py to _utils/image & improve atlas docs
    
    * Add sum_loss_list() function & correct target type hints
    
    * Add `sum_loss_list()` to `optim/_core/loss.py` with tests.
    * Replace `sum()` with `opt.loss.sum_loss_list()` in ActivationAtlas tutorial notebook.
    * Correct `target` type hints for `Loss`, `BaseLoss`, and `CompositeLoss`. The correct type hint should be `Union[nn.Module, List[nn.Module]]` as that is what the code supports and uses.
    
    * RandomRotation JIT support & other improvements
    
    * Better way to handle torch version check with JIT
    
    * The `torch.jit.is_scripting()` function isn't supported by earlier versions of PyTorch. So I've come up with a better solution that will still support earlier versions of PyTorch.
    * Exposed `align_corners` parameter to `RandomRotation` class initialization.
    * Improved `RandomRotation` class documentation.
    
    * Use better scale type hint in RandomRotation init function
    
    * Add torch.distributions support to RandomRotation
    
    * Ludwig wanted this according to his original PR.
    * Also fix mypy bug.
    
    * Add assert & more tests for RandomRotation
    
    * Added `torch.distribution` assert check and more extensive testing for the RandomRotation transform.
    
    * Adding SkipTest to RandomRotation reflection
    
    * Fix formatting error
    
    * Changes to main atlas tutorial notebook based on feedback
    
    * `extract_grid_vectors` -> `compute_avg_cell_samples`
    
    * Improved documentation, and added better descriptions to the main atlas tutorial notebook.
    
    * Remove unused type hint
    
    * Improve whitening description in main activation atlas tutorial
    
    * Spelling & grammar fixes
    
    * Improve `calc_grid_indices` documentation

--- captum/optim/__init__.py
+++ captum/optim/__init__.py
@@ -6,6 +6,7 @@ from captum.optim._core.optimization import InputOptimization  # noqa: F401
 from captum.optim._param.image import images, transforms  # noqa: F401
 from captum.optim._param.image.images import ImageTensor  # noqa: F401
 from captum.optim._utils import circuits, reducer  # noqa: F401
+from captum.optim._utils.image import atlas  # noqa: F401
 from captum.optim._utils.image.common import (  # noqa: F401
     nchannels_to_rgb,
     save_tensor_as_image,
@@ -23,6 +24,7 @@ __all__ = [
     "circuits",
     "models",
     "reducer",
+    "atlas",
     "nchannels_to_rgb",
     "save_tensor_as_image",
     "show",
--- tests/optim/core/test_loss.py
+++ tests/optim/core/test_loss.py
@@ -133,6 +133,31 @@ class TestNeuronDirection(BaseTest):
         self.assertAlmostEqual(get_loss_value(model, loss), dot, places=6)
 
 
+class TestAngledNeuronDirection(BaseTest):
+    def test_angled_neuron_direction(self) -> None:
+        model = BasicModel_ConvNet_Optim()
+        loss = opt_loss.AngledNeuronDirection(
+            model.layer, vec=torch.ones(1, 2), cossim_pow=0
+        )
+        a = 1
+        b = [CHANNEL_ACTIVATION_0_LOSS, CHANNEL_ACTIVATION_1_LOSS]
+        dot = np.sum(np.inner(a, b))
+        self.assertAlmostEqual(np.sum(get_loss_value(model, loss)), dot, places=6)
+
+    def test_angled_neuron_direction_whitened(self) -> None:
+        model = BasicModel_ConvNet_Optim()
+        loss = opt_loss.AngledNeuronDirection(
+            model.layer,
+            vec=torch.ones(1, 2),
+            vec_whitened=torch.ones(2, 2),
+            cossim_pow=0,
+        )
+        a = 1
+        b = [CHANNEL_ACTIVATION_0_LOSS, CHANNEL_ACTIVATION_1_LOSS]
+        dot = np.sum(np.inner(a, b)) * 2
+        self.assertAlmostEqual(np.sum(get_loss_value(model, loss)), dot, places=6)
+
+
 class TestTensorDirection(BaseTest):
     def test_tensor_direction(self) -> None:
         model = BasicModel_ConvNet_Optim()
@@ -242,3 +267,19 @@ class TestCompositeLoss(BaseTest):
     #         opt_loss.ChannelActivation(model.layer, 0) ** opt_loss.ChannelActivation(
     #             model.layer, 1
     #         )
+
+    def test_sum_loss_list(self) -> None:
+        n_batch = 400
+        model = torch.nn.Identity()
+        loss_fn_list = [opt_loss.LayerActivation(model) for i in range(n_batch)]
+        loss_fn = opt_loss.sum_loss_list(loss_fn_list)
+        out = get_loss_value(model, loss_fn, [n_batch, 3, 1, 1])
+        self.assertEqual(out, float(n_batch))
+
+    def test_sum_loss_list_compose_add(self) -> None:
+        n_batch = 400
+        model = torch.nn.Identity()
+        loss_fn_list = [opt_loss.LayerActivation(model) for i in range(n_batch)]
+        loss_fn = opt_loss.sum_loss_list(loss_fn_list) + opt_loss.LayerActivation(model)
+        out = get_loss_value(model, loss_fn, [n_batch, 3, 1, 1])
+        self.assertEqual(out, float(n_batch + 1.0))
