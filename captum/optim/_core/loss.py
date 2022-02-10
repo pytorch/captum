02b85674e7a244ce5533f9314937641ffb3be4f5
ProGamerGov <ProGamerGov@users.noreply.github.com>
2022-02-10 14:26:11 -0700

    Fix target bug in `sum_loss_list`

--- captum/optim/_core/loss.py
+++ captum/optim/_core/loss.py
@@ -724,7 +724,12 @@ def sum_loss_list(
     target = [
         target
         for targets in [
-            [loss.target] if not hasattr(loss.target, "__iter__") else loss.target
+            [loss.target]
+            if not (
+                hasattr(loss.target, "__iter__")
+                and not isinstance(loss.target, nn.Module)
+            )
+            else loss.target
             for loss in loss_list
         ]
         for target in targets
