81b157e55e7a2f442868b61f1caf8bf0eb2208c5
ProGamerGov <ProGamerGov@users.noreply.github.com>
2022-07-21 09:17:02 -0600

    slices -> slice

--- captum/attr/_core/neuron/neuron_integrated_gradients.py
+++ captum/attr/_core/neuron/neuron_integrated_gradients.py
@@ -92,7 +92,7 @@ class NeuronIntegratedGradients(NeuronAttribution, GradientAttribution):
                         that for all given input tensors, dimension 0 corresponds
                         to the number of examples, and if multiple input tensors
                         are provided, the examples must be aligned appropriately.
-            neuron_selector (int, callable, or tuple of int or slices):
+            neuron_selector (int, callable, or tuple of int or slice):
                         Selector for neuron
                         in given layer for which attribution is desired.
                         Neuron selector can be provided as:
