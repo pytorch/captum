03cbff1cb18d92a7e41081b0e31eef419dc025b6
Fulton Wang <fultonwang@fb.com>
2022-07-31 16:14:35 -0700

    modify tracin self influence helpers (#994)
    Summary:
    Pull Request resolved: https://github.com/pytorch/captum/pull/994
    
    change `TracInCP._self_influence_batch_tracincp` and `TracInCP._self_influence_batch_tracincp` `TracInCP._self_influence_batches_tracincp_fast` to be named `self_influence`, which is now public, and now accept a DataLoader yielding batches (as well as a single batch, as before).  The modified helper function can be called by external functions to compute self influence.
    
    The helper itself is also changed to improve efficiency, by reducing the number of times checkpoints are loaded.  The modified helper, despite being able to compute self influence scores for a dataloader yielding batches, still only loads each checkpoint once, per call.  This is because the modified helper now has an outer iteration over checkpoints, and an inner iteration over batches (the order of iteration is reversed compared to before). This helper is called by `influence` when running it in self influence mode.
    
    The reason we cannot just increase the batch size to reduce the number of checkpoint loadings is that for large models (precisely those for which loading checkpoints is expensive), the model takes up too much memory, so that the batch size cannot be too large.
    
    Minor change: for `influence_src_dataset` argument of all `__init__`'s, add description of what assumptions we make of the batches yielded by the dataloader.
    
    Differential Revision: D35603078
    
    fbshipit-source-id: 87063052e68441b82514489f4d9f9ad29b396da4

--- tests/influence/_core/test_tracin_show_progress.py
+++ tests/influence/_core/test_tracin_show_progress.py
@@ -49,115 +49,148 @@ class TestTracInShowProgress(BaseTest):
         ],
         name_func=build_test_name_func(args_to_skip=["reduction"]),
     )
-    @unittest.mock.patch("sys.stderr", new_callable=io.StringIO)
     def test_tracin_show_progress(
         self,
         reduction: str,
         tracin_constructor: Callable,
         mode: str,
-        mock_stderr,
     ) -> None:
 
-        with tempfile.TemporaryDirectory() as tmpdir:
+        with unittest.mock.patch("sys.stderr", new_callable=io.StringIO) as mock_stderr:
 
-            batch_size = 5
+            with tempfile.TemporaryDirectory() as tmpdir:
 
-            (
-                net,
-                train_dataset,
-                test_samples,
-                test_labels,
-            ) = get_random_model_and_data(
-                tmpdir, unpack_inputs=False, return_test_data=True
-            )
+                batch_size = 5
 
-            self.assertTrue(isinstance(reduction, str))
-            criterion = nn.MSELoss(reduction=reduction)
-
-            self.assertTrue(callable(tracin_constructor))
-            tracin = tracin_constructor(
-                net,
-                train_dataset,
-                tmpdir,
-                batch_size,
-                criterion,
-            )
-
-            if mode == "self influence":
-                tracin.influence(show_progress=True)
-                output = mock_stderr.getvalue()
-                self.assertTrue(
-                    (
-                        (
-                            f"Using {tracin.get_name()} to compute self influence "
-                            "for training batches: 100%"
-                        )
-                        in output
-                    ),
-                    f"Error progress output: {repr(output)}",
-                )
-            elif mode == "influence":
-
-                tracin.influence(
+                (
+                    net,
+                    train_dataset,
                     test_samples,
                     test_labels,
-                    k=None,
-                    show_progress=True,
+                ) = get_random_model_and_data(
+                    tmpdir, unpack_inputs=False, return_test_data=True
                 )
-                output = mock_stderr.getvalue()
-                self.assertTrue(
-                    (
-                        (
-                            f"Using {tracin.get_name()} to compute influence "
-                            "for training batches: 100%"
-                        )
-                        in output
-                    ),
-                    f"Error progress output: {repr(output)}",
-                )
-            elif mode == "k-most":
 
-                tracin.influence(
-                    test_samples,
-                    test_labels,
-                    k=2,
-                    proponents=True,
-                    show_progress=True,
+                self.assertTrue(isinstance(reduction, str))
+                criterion = nn.MSELoss(reduction=reduction)
+
+                self.assertTrue(callable(tracin_constructor))
+                tracin = tracin_constructor(
+                    net,
+                    train_dataset,
+                    tmpdir,
+                    batch_size,
+                    criterion,
                 )
-                output = mock_stderr.getvalue()
-                self.assertTrue(
-                    (
-                        (
-                            f"Using {tracin.get_name()} to perform computation for "
-                            "getting proponents. Processing training batches: 100%"
+
+                if mode == "self influence":
+
+                    # For self influence, displaying progress involves nested progress
+                    # bars, which are not currently supported by the backup
+                    # `SimpleProgress` that is used if `tqdm` is not installed.
+                    # Therefore, we skip the test in this case.
+                    # TODO: support nested progress bars for `SimpleProgress`
+                    try:
+                        import tqdm  # noqa
+                    except ModuleNotFoundError:
+                        raise unittest.SkipTest(
+                            (
+                                "Skipping self influence progress bar tests for "
+                                f"{tracin.get_name()}, because proper displaying "
+                                "requires the tqdm module, which is not installed."
+                            )
                         )
-                        in output
-                    ),
-                    f"Error progress output: {repr(output)}",
-                )
+
+                    tracin.influence(show_progress=True)
+                    output = mock_stderr.getvalue()
+                    # We are showing nested progress bars for the `self_influence`
+                    # method, with the outer progress bar over checkpoints, and
+                    # the inner progress bar over batches. First, we check that
+                    # the outer progress bar reaches 100% once
+                    self.assertEqual(
+                        output.count(
+                            (
+                                f"Using {tracin.get_name()} to compute self influence. "
+                                "Processing checkpoint: 100%"
+                            )
+                        ),
+                        1,
+                        f"Error in progress of batches with output: {repr(output)}",
+                    )
+                    # Second, we check that the inner progress bar reaches 100%
+                    # once for each checkpoint in `tracin.checkpoints`
+                    self.assertEqual(
+                        output.count(
+                            (
+                                f"Using {tracin.get_name()} to compute self influence. "
+                                "Processing batch: 100%"
+                            )
+                        ),
+                        len(tracin.checkpoints),
+                        f"Error in progress of checkpoints with output: {repr(output)}",
+                    )
+                elif mode == "influence":
+
+                    tracin.influence(
+                        test_samples,
+                        test_labels,
+                        k=None,
+                        show_progress=True,
+                    )
+                    output = mock_stderr.getvalue()
+                    self.assertTrue(
+                        (
+                            (
+                                f"Using {tracin.get_name()} to compute influence "
+                                "for training batches: 100%"
+                            )
+                            in output
+                        ),
+                        f"Error progress output: {repr(output)}",
+                    )
+                elif mode == "k-most":
+
+                    tracin.influence(
+                        test_samples,
+                        test_labels,
+                        k=2,
+                        proponents=True,
+                        show_progress=True,
+                    )
+                    output = mock_stderr.getvalue()
+                    self.assertTrue(
+                        (
+                            (
+                                f"Using {tracin.get_name()} to perform computation for "
+                                "getting proponents. Processing training batches: 100%"
+                            )
+                            in output
+                        ),
+                        f"Error progress output: {repr(output)}",
+                    )
+                    mock_stderr.seek(0)
+                    mock_stderr.truncate(0)
+
+                    tracin.influence(
+                        test_samples,
+                        test_labels,
+                        k=2,
+                        proponents=False,
+                        show_progress=True,
+                    )
+                    output = mock_stderr.getvalue()
+                    self.assertTrue(
+                        (
+                            (
+                                f"Using {tracin.get_name()} to perform computation for "
+                                "getting opponents. Processing training batches: 100%"
+                            )
+                            in output
+                        ),
+                        f"Error progress output: {repr(output)}",
+                    )
+                else:
+                    raise Exception("unknown test mode")
+
                 mock_stderr.seek(0)
                 mock_stderr.truncate(0)
-
-                tracin.influence(
-                    test_samples,
-                    test_labels,
-                    k=2,
-                    proponents=False,
-                    show_progress=True,
-                )
-                output = mock_stderr.getvalue()
-                self.assertTrue(
-                    (
-                        (
-                            f"Using {tracin.get_name()} to perform computation for "
-                            "getting opponents. Processing training batches: 100%"
-                        )
-                        in output
-                    ),
-                    f"Error progress output: {repr(output)}",
-                )
-            else:
-                raise Exception("unknown test mode")
-
-            mock_stderr.seek(0)
-            mock_stderr.truncate(0)
