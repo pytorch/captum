b077b6067dab20bd1bdebc032fc67f856a20e890
Aobo Yang <aoboyang@fb.com>
2022-10-03 12:44:16 -0700

    fix flake err

--- captum/_utils/gradient.py
+++ captum/_utils/gradient.py
@@ -743,8 +743,8 @@ def _compute_jacobian_wrt_params(
                 defined loss function is provided, it would be expected to be a
                 torch.nn.Module. If a custom loss is provided, it can be either type,
                 but must behave as a library loss function would if `reduction='none'`.
-        layer_modules (List[torch.nn.Module], optional): A list of PyTorch modules w.r.t. which
-                jacobian gradients are computed.
+        layer_modules (List[torch.nn.Module], optional): A list of PyTorch modules
+                 w.r.t. which jacobian gradients are computed.
     Returns:
         grads (tuple[Tensor, ...]): Returns the Jacobian for the minibatch as a
                 tuple of gradients corresponding to the tuple of trainable parameters
@@ -819,12 +819,12 @@ def _compute_jacobian_wrt_params_with_sample_wise_trick(
                 torch.nn.Module. If a custom loss is provided, it can be either type,
                 but must behave as a library loss function would if `reduction='sum'` or
                 `reduction='mean'`.
-        reduction_type (str, optional): The type of reduction applied. If a loss_fn is passed,
-                this should match `loss_fn.reduction`. Else if gradients are being
+        reduction_type (str, optional): The type of reduction applied. If a loss_fn is
+                 passed, this should match `loss_fn.reduction`. Else if gradients are being
                 computed on direct model outputs (scores), then 'sum' should be used.
                 Defaults to 'sum'.
-        layer_modules (torch.nn.Module, optional): A list of PyTorch modules w.r.t. which
-                jacobian gradients are computed.
+        layer_modules (torch.nn.Module, optional): A list of PyTorch modules w.r.t.
+                 which jacobian gradients are computed.
 
     Returns:
         grads (tuple[Tensor, ...]): Returns the Jacobian for the minibatch as a
