010f76dc36a68d62cc7ad59fc6582cfaa1d19008
Aobo Yang <aoboyang@fb.com>
2023-02-03 14:05:33 -0800

    Quiet conda install & remove conda solver (#1108)
    Summary:
    Fix the failed conda ci job which is caused by the dependencies conflicts of libmamba-solver
    - remove the libmamba-solver and use the default solver instead
    
    Also "quietly" execute `conda install` to disable progressbar, which pollutes the CircleCI logs
    
    Pull Request resolved: https://github.com/pytorch/captum/pull/1108
    
    Reviewed By: skim9
    
    Differential Revision: D43005598
    
    Pulled By: aobo-y
    
    fbshipit-source-id: 3719a1ebc7eac976759a57ed9ab455fe5d5a8908

--- scripts/install_via_conda.sh
+++ scripts/install_via_conda.sh
@@ -16,31 +16,27 @@ while getopts 'nf' flag; do
 # update conda
 # removing due to setuptools error during update
 #conda update -y -n base -c defaults conda
-conda update --all --yes
+conda update -q --all --yes
 
 # required to use conda develop
-conda install -y conda-build
-
-# Use faster conda solver
-conda install -n base conda-libmamba-solver
-conda config --set solver libmamba
+conda install -q -y conda-build
 
 # install other frameworks if asked for and make sure this is before pytorch
 if [[ $FRAMEWORKS == true ]]; then
-  pip install pytext-nlp
+  pip install -q pytext-nlp
 fi
 
 if [[ $PYTORCH_NIGHTLY == true ]]; then
   # install CPU version for much smaller download
-  conda install -y pytorch cpuonly -c pytorch-nightly
+  conda install -q -y pytorch cpuonly -c pytorch-nightly
 else
  # install CPU version for much smaller download
- conda install -y -c pytorch pytorch-cpu
+ conda install -q -y -c pytorch pytorch-cpu
 fi
 
 # install other deps
-conda install -y pytest ipywidgets ipython scikit-learn parameterized
-conda install -y -c conda-forge matplotlib pytest-cov flask flask-compress
+conda install -q -y pytest ipywidgets ipython scikit-learn parameterized
+conda install -q -y -c conda-forge matplotlib pytest-cov flask flask-compress
 
 # install captum
 python setup.py develop
