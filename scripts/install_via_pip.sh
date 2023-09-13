15e6d755038ae47a033d1fd8d54c9dd6b2ed4ab6
Vivek Miglani <vivekm@fb.com>
2023-09-13 11:48:36 -0700

    Remove sudo

--- scripts/install_via_pip.sh
+++ scripts/install_via_pip.sh
@@ -39,7 +39,7 @@ pip install --upgrade pip
 
 # install captum with dev deps
 pip install -e .[dev]
-BUILD_INSIGHTS=1 sudo python setup.py develop
+BUILD_INSIGHTS=1 python setup.py develop
 
 # install other frameworks if asked for and make sure this is before pytorch
 if [[ $FRAMEWORKS == true ]]; then
