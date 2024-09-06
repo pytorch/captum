d89243bbe0e3f8d1008703f2ccebd75b41f4da4e
Jason Cho <jjuncho@meta.com>
2024-09-06 13:27:33 -0700

    Add exceptions to callback methods (#1335)
    Summary:
    Pull Request resolved: https://github.com/pytorch/captum/pull/1335
    
    This diff adds exceptions within the callback functions for the async implementation of FeatureAblation so that it would be much easier to trace any errors within the chain of callback functions.
    
    Reviewed By: cyrjano
    
    Differential Revision: D61817023
    
    fbshipit-source-id: 1992bcf0ba23d445fdbc76298816083191e2c26e

---
+++ captum/_utils/exceptions.py
@@ -0,0 +1,11 @@
+# (c) Meta Platforms, Inc. and affiliates. Confidential and proprietary.
+
+# pyre-strict
+
+
+class FeatureAblationFutureError(Exception):
+    """This custom error is raised when an error
+    occurs within the callback chain of a
+    FeatureAblation attribution call"""
+
+    pass
