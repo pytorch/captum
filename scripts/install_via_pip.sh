a3421b487a4243b98a2a6d7b5ee890c0ef3239b7
Zach Carmichael <craymichael@meta.com>
2024-10-08 16:19:45 -0700

    Test against multiple version of transformers library (#1362)
    Summary:
    
    On OSS, test against multiple versions of transformers library. Versions selected based on caching logic changes between versions, as well as the newest version.
    
    Differential Revision: D64075428

--- scripts/install_via_pip.sh
+++ scripts/install_via_pip.sh
@@ -5,14 +5,16 @@ set -e
 PYTORCH_NIGHTLY=false
 DEPLOY=false
 CHOSEN_TORCH_VERSION=-1
+CHOSEN_TRANSFORMERS_VERSION=-1
 
-while getopts 'ndfv:' flag; do
+while getopts 'ndfv:t:' flag; do
   case "${flag}" in
     n) PYTORCH_NIGHTLY=true ;;
     d) DEPLOY=true ;;
     f) FRAMEWORKS=true ;;
     v) CHOSEN_TORCH_VERSION=${OPTARG};;
-    *) echo "usage: $0 [-n] [-d] [-f] [-v version]" >&2
+    t) CHOSEN_TRANSFORMERS_VERSION=${OPTARG};;
+    *) echo "usage: $0 [-n] [-d] [-f] [-v version] [-t transformers_version]" >&2
        exit 1 ;;
     esac
   done
@@ -66,4 +68,10 @@ if [[ $DEPLOY == true ]]; then
   pip install beautifulsoup4 ipython nbconvert==5.6.1 --progress-bar off
 fi
 
-pip install transformers --progress-bar off
+# install appropriate transformers version
+# If no version is specified, upgrade to the latest release.
+if [[ $CHOSEN_TRANSFORMERS_VERSION == -1 ]]; then
+  pip install --upgrade transformers --progress-bar off
+else
+  pip install transformers=="$CHOSEN_TRANSFORMERS_VERSION" --progress-bar off
+fi
