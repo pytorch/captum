067ee6cffb8073342f31f4da0a141264d3b0cc25
ProGamerGov <ProGamerGov@users.noreply.github.com>
2025-03-30 12:17:25 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@
 #   BUILD_INSIGHTS
 #     enables Captum Insights build via yarn
 #
-b
+
 import os
 import re
 import subprocess
@@ -16,7 +16,7 @@ import sys
 from setuptools import find_packages, setup
 
 REQUIRED_MAJOR = 3
-REQUIRED_MINOR = 6
+REQUIRED_MINOR = 9
 
 # Check for python version
 if sys.version_info < (REQUIRED_MAJOR, REQUIRED_MINOR):
@@ -61,15 +61,15 @@ INSIGHTS_FILE_SUBDIRS = [
 
 TUTORIALS_REQUIRES = INSIGHTS_REQUIRES + ["torchtext", "torchvision"]
 
-TEST_REQUIRES = ["pytest", "pytest-cov", "parameterized"]
+TEST_REQUIRES = ["pytest", "pytest-cov", "parameterized", "flask", "flask-compress"]
 
 DEV_REQUIRES = (
     INSIGHTS_REQUIRES
     + TEST_REQUIRES
     + [
-        "black==22.3.0",
+        "black",
         "flake8",
-        "sphinx",
+        "sphinx<8.2.0",
         "sphinx-autodoc-typehints",
         "sphinxcontrib-katex",
         "mypy>=0.760",
@@ -82,7 +82,9 @@ DEV_REQUIRES = (
 
 # get version string from module
 with open(os.path.join(os.path.dirname(__file__), "captum/__init__.py"), "r") as f:
-    version = re.search(r"__version__ = ['\"]([^'\"]*)['\"]", f.read(), re.M).group(1)
+    version_match = re.search(r"__version__ = ['\"]([^'\"]*)['\"]", f.read(), re.M)
+    assert version_match is not None, "Unable to find version string."
+    version = version_match.group(1)
     report("-- Building version " + version)
 
 # read in README.md as the long description
@@ -131,13 +133,16 @@ if __name__ == "__main__":
         keywords=[
             "Model Interpretability",
             "Model Understanding",
+            "Model Interpretability",
+            "Model Understanding",
             "Feature Importance",
             "Neuron Importance",
-            "Feature Visualization",
+            "Data Attribution",
+            "Explainable AI",
             "PyTorch",
         ],
         classifiers=[
-            "Development Status :: 4 - Beta",
+            "Development Status :: 5 - Production/Stable",
             "Intended Audience :: Developers",
             "Intended Audience :: Education",
             "Intended Audience :: Science/Research",
@@ -147,8 +152,17 @@ if __name__ == "__main__":
         ],
         long_description=long_description,
         long_description_content_type="text/markdown",
-        python_requires=">=3.6",
-        install_requires=["matplotlib", "numpy", "packaging", "torch>=1.6"],
+        python_requires=">={required_major}.{required_minor}".format(
+            required_minor=REQUIRED_MINOR,
+            required_major=REQUIRED_MAJOR,
+        ),
+        install_requires=[
+            "matplotlib",
+            "numpy<2.0",
+            "packaging",
+            "torch>=1.10",
+            "tqdm",
+        ],
         packages=find_packages(exclude=("tests", "tests.*")),
         extras_require={
             "dev": DEV_REQUIRES,
