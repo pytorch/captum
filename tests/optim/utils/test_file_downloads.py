acb7c13fc37fc59a9a69801baf84201c5cbe1fd2
ProGamerGov <ProGamerGov@users.noreply.github.com>
2025-04-03 13:53:02 -0600

    Update test_file_downloads.py

--- tests/optim/utils/test_file_downloads.py
+++ tests/optim/utils/test_file_downloads.py
@@ -1,65 +1,112 @@
 #!/usr/bin/env python3
 import unittest
-import torch
+
 import requests
+import torch
 
 BASE_URL = "https://pytorch-tutorial-assets.s3.amazonaws.com/captum/"
 BASE_URL_MODELS = "https://pytorch.s3.amazonaws.com/models/captum/"
 
+
 class TestFileExistence(unittest.TestCase):
     def test_clip_bpe_simple_vocab_48895_txt(self) -> None:
-        url = BASE_UR_MODELSL + "clip_bpe_simple_vocab_48895.txt"
+        url = BASE_URL_MODELS + "clip_bpe_simple_vocab_48895.txt"
         response = requests.get(url)
         self.assertEqual(response.status_code, 200, f"File not found: {url}")
         lines = response.text.strip().split("\n")
-        self.assertEqual(len(lines), 48895, f"Unexpected number of lines in {url}," +  f"expected 48895 but got {len(lines)}")
+        self.assertEqual(
+            len(lines),
+            48895,
+            f"Unexpected number of lines in {url},"
+            + f"expected 48895 but got {len(lines)}",
+        )
 
     def test_inceptionv1_mixed4c_relu_samples_activations_pt(self) -> None:
         url = BASE_URL + "inceptionv1_mixed4c_relu_samples_activations.pt"
-        state_dict = torch.hub.load_state_dict_from_url(url, progress=False, check_hash=False)
-        self.assertTrue(isinstance(state_dict, dict), f"Failed to load state dict from: {url}")
+        state_dict = torch.hub.load_state_dict_from_url(
+            url, progress=False, check_hash=False
+        )
+        self.assertTrue(
+            isinstance(state_dict, dict), f"Failed to load state dict from: {url}"
+        )
 
     def test_inceptionv1_mixed4c_relu_samples_attributions_pt(self) -> None:
         url = BASE_URL + "inceptionv1_mixed4c_relu_samples_attributions.pt"
-        state_dict = torch.hub.load_state_dict_from_url(url, progress=False, check_hash=False)
-        self.assertTrue(isinstance(state_dict, dict), f"Failed to load state dict from: {url}")
+        state_dict = torch.hub.load_state_dict_from_url(
+            url, progress=False, check_hash=False
+        )
+        self.assertTrue(
+            isinstance(state_dict, dict), f"Failed to load state dict from: {url}"
+        )
 
     def test_inceptionv1_mixed5b_relu_samples_activations_pt(self) -> None:
         url = BASE_URL + "inceptionv1_mixed5b_relu_samples_activations.pt"
-        state_dict = torch.hub.load_state_dict_from_url(url, progress=False, check_hash=False)
-        self.assertTrue(isinstance(state_dict, dict), f"Failed to load state dict from: {url}")
+        state_dict = torch.hub.load_state_dict_from_url(
+            url, progress=False, check_hash=False
+        )
+        self.assertTrue(
+            isinstance(state_dict, dict), f"Failed to load state dict from: {url}"
+        )
 
     def test_inceptionv1_mixed5b_relu_samples_attributions_pt(self) -> None:
         url = BASE_URL + "inceptionv1_mixed5b_relu_samples_attributions.pt"
-        state_dict = torch.hub.load_state_dict_from_url(url, progress=False, check_hash=False)
-        self.assertTrue(isinstance(state_dict, dict), f"Failed to load state dict from: {url}")
+        state_dict = torch.hub.load_state_dict_from_url(
+            url, progress=False, check_hash=False
+        )
+        self.assertTrue(
+            isinstance(state_dict, dict), f"Failed to load state dict from: {url}"
+        )
 
     def test_inception5h_pth(self) -> None:
-        url = BASE_UR_MODELSL + "inception5h.pth"
-        state_dict = torch.hub.load_state_dict_from_url(url, progress=False, check_hash=False)
-        self.assertTrue(isinstance(state_dict, dict), f"Failed to load state dict from: {url}")
+        url = BASE_URL_MODELS + "inception5h.pth"
+        state_dict = torch.hub.load_state_dict_from_url(
+            url, progress=False, check_hash=False
+        )
+        self.assertTrue(
+            isinstance(state_dict, dict), f"Failed to load state dict from: {url}"
+        )
 
     def test_inceptionv1_places365_pth(self) -> None:
         url = BASE_URL + "inceptionv1_places365.pth"
-        state_dict = torch.hub.load_state_dict_from_url(url, progress=False, check_hash=False)
-        self.assertTrue(isinstance(state_dict, dict), f"Failed to load state dict from: {url}")
+        state_dict = torch.hub.load_state_dict_from_url(
+            url, progress=False, check_hash=False
+        )
+        self.assertTrue(
+            isinstance(state_dict, dict), f"Failed to load state dict from: {url}"
+        )
 
     def test_vgg16_caffe_features_pth(self) -> None:
         url = BASE_URL + "vgg16_caffe_features.pth"
-        state_dict = torch.hub.load_state_dict_from_url(url, progress=False, check_hash=False)
-        self.assertTrue(isinstance(state_dict, dict), f"Failed to load state dict from: {url}")
+        state_dict = torch.hub.load_state_dict_from_url(
+            url, progress=False, check_hash=False
+        )
+        self.assertTrue(
+            isinstance(state_dict, dict), f"Failed to load state dict from: {url}"
+        )
 
     def test_clip_resnet50x4_facets_pt(self) -> None:
-        url = BASE_UR_MODELSL + "clip_resnet50x4_facets.pt"
-        state_dict = torch.hub.load_state_dict_from_url(url, progress=False, check_hash=False)
-        self.assertTrue(isinstance(state_dict, dict), f"Failed to load state dict from: {url}")
+        url = BASE_URL_MODELS + "clip_resnet50x4_facets.pt"
+        state_dict = torch.hub.load_state_dict_from_url(
+            url, progress=False, check_hash=False
+        )
+        self.assertTrue(
+            isinstance(state_dict, dict), f"Failed to load state dict from: {url}"
+        )
 
     def test_clip_resnet50x4_image_pt(self) -> None:
-        url = BASE_UR_MODELSL + "clip_resnet50x4_image.pt"
-        state_dict = torch.hub.load_state_dict_from_url(url, progress=False, check_hash=False)
-        self.assertTrue(isinstance(state_dict, dict), f"Failed to load state dict from: {url}")
+        url = BASE_URL_MODELS + "clip_resnet50x4_image.pt"
+        state_dict = torch.hub.load_state_dict_from_url(
+            url, progress=False, check_hash=False
+        )
+        self.assertTrue(
+            isinstance(state_dict, dict), f"Failed to load state dict from: {url}"
+        )
 
     def test_clip_resnet50x4_text_pt(self) -> None:
-        url = BASE_UR_MODELSL + "clip_resnet50x4_text.pt"
-        state_dict = torch.hub.load_state_dict_from_url(url, progress=False, check_hash=False)
-        self.assertTrue(isinstance(state_dict, dict), f"Failed to load state dict from: {url}")
+        url = BASE_URL_MODELS + "clip_resnet50x4_text.pt"
+        state_dict = torch.hub.load_state_dict_from_url(
+            url, progress=False, check_hash=False
+        )
+        self.assertTrue(
+            isinstance(state_dict, dict), f"Failed to load state dict from: {url}"
+        )
