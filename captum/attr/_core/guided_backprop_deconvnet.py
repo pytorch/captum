Fix Guided Backprop pyre fixme issues (#1605)
Summary:
Pull Request resolved: https://github.com/pytorch/captum/pull/1605

Fixing unresolved pyre fixme issues in corresponding file

Reviewed By: styusuf

Differential Revision: D76737312

fbshipit-source-id: 9f1bd322315d423eeee3cb622373fec2bbbd5e5f

captum/attr/_core/guided_backprop_deconvnet.py
@@ -2,7 +2,7 @@
 
 # pyre-strict
 import warnings
-from typing import Callable, List, Optional, Tuple, Union
+from typing import cast, List, Optional, Tuple, Union
 
 import torch
 import torch.nn.functional as F
@@ -78,12 +78,11 @@ class ModifiedReluGradientAttribution(GradientAttribution):
             self._remove_hooks()
 
         undo_gradient_requirements(inputs_tuple, gradient_mask)
-        # pyre-fixme[7]: Expected `TensorOrTupleOfTensorsGeneric` but got
-        #  `Tuple[Tensor, ...]`.
-        return _format_output(is_inputs_tuple, gradients)
+        return cast(
+            TensorOrTupleOfTensorsGeneric, _format_output(is_inputs_tuple, gradients)
+        )
 
-    # pyre-fixme[24] Generic type `Callable` expects 2 type parameters.
-    def attribute_future(self) -> Callable:
+    def attribute_future(self) -> None:
         r"""
         This method is not implemented for ModifiedReluGradientAttribution.
         """