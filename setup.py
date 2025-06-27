a7573fa0136b58ffec79ae5f338763afdb8fb803
Zach Carmichael <craymichael@meta.com>
2025-06-26 20:19:43 -0700

    bump OSS pyre nightly version (#1621)
    Summary:
    Pull Request resolved: https://github.com/pytorch/captum/pull/1621
    
    OSS pyre with bumped version is in alignment with internal pyre
    
    Reviewed By: Ayush-Warikoo
    
    Differential Revision: D77403529
    
    fbshipit-source-id: 5adbfbdd67dda13b94f2a63b4fb7fa68667620fe

--- setup.py
+++ setup.py
@@ -76,7 +76,7 @@ DEV_REQUIRES = (
         "sphinx-autodoc-typehints",
         "sphinxcontrib-katex",
         "mypy>=0.760",
-        "pyre-check-nightly==0.0.101745838703",
+        "pyre-check-nightly==0.0.101750936314",
         "usort==1.0.2",
         "ufmt",
         "scikit-learn",
